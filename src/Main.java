d7b81894293ca6c82749399e3a9047ca3209cdd7
redggum <redggum@naver.com>
2018-10-30 01:09:25 +0900

    init_2nd

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/* 1043번 거짓말 */
 
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	static int N, M;
+	static int truth_man_cnt;
+	static int[] truth_man;
+	
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] strs;
+		
+		strs = br.readLine().split(" ");
+		
+		N = Integer.parseInt(strs[0]);
+		M = Integer.parseInt(strs[1]);
+		
+		
 	}
 
 }
