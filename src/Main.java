51a8cab590644107b1f378a6828d6e0d85fb9091
redggum <redggum@naver.com>
2018-11-10 01:44:42 +0900

    successgit add .

--- src/Main.java
+++ src/Main.java
@@ -1,74 +1,51 @@
-	import java.io.BufferedReader;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-	
-	/* 1701번 Cubeditor */
-	
-	public class Main {
-	
-		static String T;
-		static int[] pi; 
-		
-		public static void main(String[] args) throws IOException {
-			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-			
-			T = br.readLine();
-	
-			char[] tc = T.toCharArray();
-			
-			int pIdx = 0;
-			int[] matchPos = new int[tc.length];
-			int matchCnt = 0;
-			pi = new int[P.length()];
-			int matchBegin = 0;
-			
-			calcPI(P);
-			
-			for (int i = 0; i < tc.length; i++) {
-	//			System.out.println("tc[" + i + "] : " + tc[i] + ", tp[" + pIdx + "] : " + tp[pIdx]);
-				
-				if (tc[i] == tp[pIdx]) {
-					pIdx++;
-				} else {
-	//				System.out.println("pIdx : " + pIdx);
-					
-					if (pIdx > 0) {
-	//					System.out.println("pi[" + (pIdx - 1) + "] : " + pi[pIdx - 1]);				
-						pIdx = pi[pIdx - 1];
-						i--;
-					}
-				}
-				
-				if (pIdx == tp.length) {
-	//				System.out.println("Matched!!!!!, i : " + i);
-					matchBegin = (i + 1) - (tp.length - 1); 
-					matchPos[matchCnt++] = matchBegin;
-					pIdx = pi[tp.length - 1];
-				}
-			}
-			
-			System.out.println(matchCnt);
-			
-			for (int i = 0; i < matchCnt; i++) {
-				System.out.print(matchPos[i] + " ");
-			}
-			
+import java.util.Arrays;
+
+/* 1701번 Cubeditor */
+
+public class Main {
+
+	static String T;
+	static int[] pi;
+	static int max = 0;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		T = br.readLine();
+
+		char[] tc = T.toCharArray();
+
+		pi = new int[T.length()];
+
+		for (int i = 0; i < tc.length; i++) {
+			Arrays.fill(pi, 0);
+			calcPI(T.substring(i, T.length()));
 		}
-		
-		static void calcPI(String pattern) {
-			char[] p = pattern.toCharArray();
-			
-			int j = 0;
-			
-			for (int i = 1; i < pattern.length(); i++) {
-				while(j > 0 && p[j] != p[i]) {
-					j = pi[j - 1];
-				}
-				
-				if (p[j] == p[i]) {
-					pi[i] = ++j;
-				}
-			}
-		}
-	
+
+		System.out.println(max);
+
 	}
+
+	static void calcPI(String pattern) {
+		char[] p = pattern.toCharArray();
+//		System.out.println(p);
+
+		int j = 0;
+
+		for (int i = 1; i < pattern.length(); i++) {
+			while (j > 0 && p[j] != p[i]) {
+				j = pi[j - 1];
+			}
+
+			if (p[j] == p[i]) {
+				pi[i] = ++j;
+
+				max = (max < pi[i]) ? pi[i] : max;
+			}
+		}
+	}
+
+}
