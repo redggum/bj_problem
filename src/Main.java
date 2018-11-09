131616a2451afdfdcafe708dce8ecd9b229792e6
redggum <redggum@naver.com>
2018-11-10 01:18:12 +0900

    init

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,20 @@
 	import java.io.BufferedReader;
-	import java.io.IOException;
-	import java.io.InputStreamReader;
-	import java.util.ArrayList;
-	import java.util.Arrays;
+import java.io.IOException;
+import java.io.InputStreamReader;
 	
-	/* 1786번 찾기 */
+	/* 1701번 Cubeditor */
 	
 	public class Main {
 	
-		static String T, P;
+		static String T;
 		static int[] pi; 
 		
 		public static void main(String[] args) throws IOException {
 			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 			
 			T = br.readLine();
-			P = br.readLine();
 	
 			char[] tc = T.toCharArray();
-			char[] tp = P.toCharArray();
 			
 			int pIdx = 0;
 			int[] matchPos = new int[tc.length];
