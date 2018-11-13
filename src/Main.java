335a0a282c7f20d61dd8d8ff9f2c129dee4d6231
redggum <redggum@naver.com>
2018-11-14 00:23:28 +0900

    failed...

--- src/Main.java
+++ src/Main.java
@@ -2,126 +2,127 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.PriorityQueue;
+import java.util.Stack;
 
 /* 1506번 경찰서 */
 
 public class Main {
 
-	static int N, M, C;
+	static int N;	// 1 <= N <= 100
+	static int[] cost;
+	static boolean[][] adj;
+	static Stack<Integer> stack;
+	static ArrayList<PriorityQueue<Node>> al;
+	static int[] visited;
+	static int sum;
+	static int cnt;
 	
 	public static void main(String[] args) throws IOException {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		N = Integer.parseInt(br.readLine());
+		
+		cost = new int[N + 1];
+		
 		String[] strs = br.readLine().split(" ");
-		int s, e, d;
-		PriorityQueue<Node> pq = new PriorityQueue<Node>();
-		Node node;
-		int v;
-		int[] dist;
-		int nextV;
-		int adj;
-		long sum = 0;
-		long result = 0;
-		long calc = 0;
-		boolean[] check;
+		String str;
 		
-		N = Integer.parseInt(strs[0]);
-		M = Integer.parseInt(strs[1]);
-		C = Integer.parseInt(strs[2]);
-		
-		ArrayList<ArrayList<Adj>> al = new ArrayList<ArrayList<Adj>>();
-		dist = new int[N];
-		check = new boolean[N];
-		
-		Arrays.fill(dist, Integer.MAX_VALUE);
-		dist[0] = 0;
-		
-		for (int i = 0; i < N; i++) {
-			al.add(new ArrayList<Adj>());
+		for (int i = 1; i <= N; i++) {
+			cost[i] = Integer.parseInt(strs[i - 1]);
 		}
 		
-		for (int i = 0; i < M; i++) {
-			strs = br.readLine().split(" ");
-			
-			s = Integer.parseInt(strs[0]);
-			e = Integer.parseInt(strs[1]);
-			d = Integer.parseInt(strs[2]);
-			
-			al.get(s - 1).add(new Adj(e, d));
-			al.get(e - 1).add(new Adj(s, d));
-			sum += d;
-		}
+		adj = new boolean[N + 1][N + 1];
 		
-		result = sum;
-		
-		pq.add(new Node(1, 0));
-		
-		while(!pq.isEmpty()) {
-			node = pq.poll();
-			v = node.v;
+		for (int i = 1; i <= N; i++) {
+			str = br.readLine();
 			
-			if (check[v - 1]) {
-				continue;
-			}
-			
-			check[v - 1] = true;
-			
-//			System.out.println("pulled v : " + v);
-			
-			for(int i = 0; i < al.get(v - 1).size(); i++) {
-				nextV = al.get(v - 1).get(i).v;
-				adj = al.get(v - 1).get(i).adj;
-				
-				if (check[nextV - 1]) {
-					sum -= adj;
-					continue;
-				}
-				
-				if (dist[nextV - 1] == Integer.MAX_VALUE || dist[v - 1] + adj < dist[nextV - 1]) {
-					dist[nextV - 1] = dist[v - 1] + adj;
-					
-//					System.out.println("queue add : dist[" + (nextV - 1) + "] : " + dist[nextV - 1]);
-					
-					pq.add(new Node(nextV, dist[nextV - 1]));
+			for (int j = 1; j <= N; j++) {
+				if (str.charAt(j - 1) == '1') {
+					adj[i][j] = true;
 				}
 			}
+		}
+		
+		
+		for (int v = 1; v <= N; v++) {
+			stack = new Stack<Integer>();
+			visited = new int[N + 1];
+			al = new ArrayList<PriorityQueue<Node>>();
+			cnt = 0;
+			sum = 0;
 			
-			calc = C * dist[v - 1] + sum;
+			stack.push(v);
 			
-//			System.out.println("calc : " + calc + ", C : " + C + ", dist[" + (v - 1) + "] : " + dist[v - 1] + ", sum : " + sum);
+			findCycle(v);
 			
-			if (result > calc) {
-				result = calc;
+			for (int i = 0; i < al.size(); i++) {
+				sum += al.get(i).poll().cost;
+			}
+			
+			while(!stack.isEmpty()) {
+				sum += cost[stack.pop()];
+				cnt++;
+			}
+			
+			if (cnt == N) {
+				break;
 			}
 		}
 		
-		System.out.println(result);
+		System.out.println(sum);
 	}
-}
-
-class Adj {
-	int v;
-	int adj;
 	
-	Adj(int v, int adj) {
-		this.v = v;
-		this.adj = adj;
+	static void findCycle(int node) {
+		if (visited[node] != 0) {
+			if (visited[node] == -1) {	// cycle
+				int pop = 0;
+				PriorityQueue<Node> pq = new PriorityQueue<Node>();
+				while(stack.contains(node)) {
+					pop = stack.pop();
+					pq.add(new Node(pop, cost[pop]));
+					cnt++;
+				}
+				
+				al.add(pq);
+				
+				return;
+			}
+			
+			return;
+		}
+		
+		visited[node] = -1;
+		
+		for (int i = 1; i <= N; i++) {
+			
+			if (adj[node][i]) {
+//				System.out.println("node : " + node + ", child : " + i);
+				
+				stack.push(i);
+				findCycle(i);
+			}
+		}
+		
+		visited[node] = 1;
+		
+		return;
 	}
 }
 
 class Node implements Comparable<Node> {
 	int v;
-	int dist;
+	int cost;
 	
-	Node(int v, int dist) {
+	Node(int v, int cost) {
 		this.v = v;
-		this.dist = dist;
+		this.cost = cost;
 	}
 
 	@Override
 	public int compareTo(Node o) {
-		return this.dist >= o.dist ? 1 : -1;
+		return this.cost >= o.cost ? 1 : -1;
 	}
 }
