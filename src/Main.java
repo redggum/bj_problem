06da2c9f07fb61245fc2966357156261c2f74c86
redggum <redggum@naver.com>
2018-11-17 00:34:06 +0900

    successgit add .git add .

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,14 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.PriorityQueue;
-import java.util.Stack;
 
-/* 1574번 룩 어택 */
+/* 1412번 일방통행 */
 
 public class Main {
 
 	static int N;	// 1 <= N <= 100
-	static int[] cost;
 	static boolean[][] adj;
-	static Stack<Integer> stack;
-	static ArrayList<PriorityQueue<Node>> al;
 	static int[] visited;
-	static int sum;
-	static int cnt;
 	
 	public static void main(String[] args) throws IOException {
 		
@@ -25,73 +16,44 @@ public class Main {
 		
 		N = Integer.parseInt(br.readLine());
 		
-		cost = new int[N + 1];
-		
-		String[] strs = br.readLine().split(" ");
-		String str;
-		
-		for (int i = 1; i <= N; i++) {
-			cost[i] = Integer.parseInt(strs[i - 1]);
-		}
-		
 		adj = new boolean[N + 1][N + 1];
 		
+		String[] strs;
+		
 		for (int i = 1; i <= N; i++) {
-			str = br.readLine();
+			strs = br.readLine().split("");
 			
 			for (int j = 1; j <= N; j++) {
-				if (str.charAt(j - 1) == '1') {
-					adj[i][j] = true;
+				if (strs[j - 1].equals("Y")) {
+					if (adj[j][i] == false) {
+						adj[i][j] = true;
+					} else {
+						// if the edge is bidirectional, don't care of checking cycle
+						adj[j][i] = false;
+					}
 				}
 			}
 		}
-		
 		
 		for (int v = 1; v <= N; v++) {
-			stack = new Stack<Integer>();
 			visited = new int[N + 1];
-			al = new ArrayList<PriorityQueue<Node>>();
-			cnt = 0;
-			sum = 0;
-			
-			stack.push(v);
-			
-			findCycle(v);
-			
-			for (int i = 0; i < al.size(); i++) {
-				sum += al.get(i).poll().cost;
-			}
-			
-			while(!stack.isEmpty()) {
-				sum += cost[stack.pop()];
-				cnt++;
-			}
-			
-			if (cnt == N) {
-				break;
+
+			if (findCycle(v)) {
+				System.out.println("NO");
+				return;
 			}
 		}
 		
-		System.out.println(sum);
+		System.out.println("YES");
 	}
 	
-	static void findCycle(int node) {
+	static boolean findCycle(int node) {
 		if (visited[node] != 0) {
 			if (visited[node] == -1) {	// cycle
-				int pop = 0;
-				PriorityQueue<Node> pq = new PriorityQueue<Node>();
-				while(stack.contains(node)) {
-					pop = stack.pop();
-					pq.add(new Node(pop, cost[pop]));
-					cnt++;
-				}
-				
-				al.add(pq);
-				
-				return;
+				return true;
 			}
 			
-			return;
+			return false;
 		}
 		
 		visited[node] = -1;
@@ -100,15 +62,15 @@ public class Main {
 			
 			if (adj[node][i]) {
 //				System.out.println("node : " + node + ", child : " + i);
-				
-				stack.push(i);
-				findCycle(i);
+				if (findCycle(i)) {
+					return true;
+				}
 			}
 		}
 		
 		visited[node] = 1;
 		
-		return;
+		return false;
 	}
 }
 
