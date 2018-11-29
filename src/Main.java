48d317a9943aabfa8b21a481f466a33926536c70
redggum <redggum@naver.com>
2018-11-30 01:01:11 +0900

    init

--- src/Main.java
+++ src/Main.java
@@ -2,39 +2,22 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-/* 1476번 날짜 계산 */
+/* 1977번 완전제곱수 */
 
 public class Main {
 
-	static int E, S, M;	// 1 ≤ E ≤ 15, 1 ≤ S ≤ 28, 1 ≤ M ≤ 19
-	static final int E_MOD = 15;
-	static final int S_MOD = 28;
-	static final int M_MOD = 19;
-	static int year = 0;
+	static int M, N;	// 1 ≤ M <= N ≤ 10,000
+	static final int SUM = 0;
+	static final int MIN = Integer.MAX_VALUE;
 	
 	public static void main(String[] args) throws IOException {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String[] strs;
 		
-		strs = br.readLine().split(" ");
-		E = Integer.parseInt(strs[0]);
-		S = Integer.parseInt(strs[1]);
-		M = Integer.parseInt(strs[2]);
+		M = Integer.parseInt(br.readLine());
+		N = Integer.parseInt(br.readLine());
 
-		year = E;
-		
-		while(true) {
-//			System.out.println("year : " + year);
-			if ( ((year % S_MOD == S) || (year % S_MOD == 0 && S_MOD == S)) && 
-					(year % M_MOD == M || (year % M_MOD == 0 && M_MOD == M)) ) {
-//				System.out.println("Find!!");
-				break;
-			}
-			
-			year += E_MOD;
-		}
-		
-		System.out.println(year);
+		System.out.println(SUM);
+		System.out.println(MIN);
 	}
 }
