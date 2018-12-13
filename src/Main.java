58a6b5ef4b8538be5a5f236188ccee5dc1895c91
redggum <redggum@naver.com>
2018-12-14 01:15:02 +0900

    init

--- src/Main.java
+++ src/Main.java
@@ -2,29 +2,29 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-/* 2864번 5와 6의 차이 */
+/* 1107번 리모컨 */
 
 public class Main {
 
-	static long A, B; // A, B <= 1,000,000
-	static long minA, minB, maxA, maxB;
+	static int N; // 0 <= N <= 500,000
+	static int M; // 0 <= N <= 10
+	static int S = 100;
+	static boolean[] brokenBtn = new boolean[10];
+	static int cnt = 0;
 
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String[] strs = br.readLine().split(" ");
-		A = Long.parseLong(strs[0]);
-		B = Long.parseLong(strs[1]);
-
-		String str1 = String.valueOf(A);
-		String str2 = String.valueOf(B);
-
-		minA = Long.parseLong(str1.replaceAll("6", "5"));
-		minB = Long.parseLong(str2.replaceAll("6", "5"));
-
-		maxA = Long.parseLong(str1.replaceAll("5", "6"));
-		maxB = Long.parseLong(str2.replaceAll("5", "6"));
 		
-		System.out.println((minA + minB) + " " + (maxA + maxB));
+		N = Integer.parseInt(br.readLine());
+		M = Integer.parseInt(br.readLine());
+		
+		String[] strs = br.readLine().split(" ");
+		
+		for (int i = 0; i < strs.length; i++) {
+			brokenBtn[Integer.parseInt(strs[i])] = true;
+		}
+		
+		
 	}
 }
