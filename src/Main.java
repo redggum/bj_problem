503ab9aab7bedc041f7394cc6e10ceb67b38e064
redggum <redggum@naver.com>
2018-12-13 23:01:23 +0900

    success

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,105 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 /* 1652번 누울 자리를 찾아라 */
 
 public class Main {
 
-	static long A, B, V;	// 1 <= B < A <= V <= 1,000,000,000
+	static int N;	// 1 <= N <= 100
+	static char[][] arr;
+	static int cnt;
+	static int len;
+	static char ch;
+	static int x, y;
+	static boolean trigger;
 	
 	public static void main(String[] args) throws IOException {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] strs = null;
 		
-		String[] strs = br.readLine().split(" ");
+		N = Integer.parseInt(br.readLine());
 		
-		A = Long.parseLong(strs[0]);
-		B = Long.parseLong(strs[1]);
-		V = Long.parseLong(strs[2]);
+		arr = new char[N + 1][N + 1];
+		cnt = 0;
+		x = 0;
+		y = 0;
 		
-		long reminder = (V - A) % (A - B);
 		
-		long plus = (reminder > 0 ? 2 : 1);
+		for (int i = 1; i <= N; i++) {
+			strs = br.readLine().split("");
+			
+			for (int j = 1; j <= N; j++) {
+				ch = strs[j - 1].charAt(0);
+				arr[i][j] = ch;
+			}
+		}
 		
-		System.out.println((V - A) / (A - B) + plus);
+		x = cnt;
+		cnt = 0;
+		
+		System.out.println(xy_calc() + " " + yx_calc());
+	}
+	
+	static int xy_calc() {
+		int cnt = 0;
+		char ch;
+		
+		for (int i = 1; i <= N; i++) {		
+			len = 0;
+			ch = 0;
+			trigger = false;
+			
+			for (int j = 1; j <= N; j++) {
+				ch = arr[i][j];
+				if (ch == '.') {
+					 if (trigger == false) {
+						 len++;
+					 }
+				} else {
+					len = 0;
+					trigger = false;
+				}
+				
+				if (len == 2 && trigger == false) {
+					cnt++;
+					trigger = true;
+				}
+			}
+		}
+		
+		return cnt;
+	}
+	
+	static int yx_calc() {
+		int cnt = 0;
+		char ch;
+	
+		for (int i = 1; i <= N; i++) {		
+			len = 0;
+			ch = 0;
+			trigger = false;
+			
+			for (int j = 1; j <= N; j++) {
+				ch = arr[j][i];
+				if (ch == '.') {
+					 if (trigger == false) {
+						 len++;
+					 }
+				} else {
+					len = 0;
+					trigger = false;
+				}
+				
+				if (len == 2 && trigger == false) {
+					cnt++;
+					trigger = true;
+				}
+			}
+		}
+		
+		return cnt;
 	}
 }
