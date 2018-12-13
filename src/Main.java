c75fe664899b26e7a310d1ccb1635b0fe983ae4d
redggum <redggum@naver.com>
2018-12-14 00:26:19 +0900

    success

--- src/Main.java
+++ src/Main.java
@@ -2,53 +2,30 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-/* 1074번 Z */
+/* 1075번 나누기 */
 
 public class Main {
 
-	static int N, r, c; // 1 <= N <= 15, 0 <= r, c <= 2^N-1
-	static int n;
-	static int[] X = { 0, 1, 0, 1 };
-	static int[] Y = { 0, 0, 1, 1 };
+	static long N, F; // 100 <= N <= 2,000,000,000, 1 <= F <= 100
+	static long s;
+	static long v;
 
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String[] strs = null;
 
-		strs = br.readLine().split(" ");
-		N = Integer.parseInt(strs[0]);
-		r = Integer.parseInt(strs[1]);
-		c = Integer.parseInt(strs[2]);
-
-		n = (int) Math.pow(2, N);
-//		System.out.println(n);
-
-		System.out.println(move(r, c) - 1);
-	}
-
-	public static long move(int row, int col) {
-		int size = n;
-		int nextRow = 0;
-		int nextCol = 0;
-		long cnt = 0;
-
-		for (int y = 0; y < size; y += 2) {
-			for (int x = 0; x < size; x += 2) {
-				for (int z = 0; z < 4; z++) {
-					nextRow = y + Y[z];
-					nextCol = x + X[z];
-					cnt++;
-
-//					System.out.println("nextRow : " + nextRow + ", nextCol : " + nextCol + ", cnt : " + cnt);
-
-					if (nextRow == row && nextCol == col) {
-						return cnt;
-					}
-				}
+		N = Long.parseLong(br.readLine());
+		F = Long.parseLong(br.readLine());
+		
+		s = (N / 100) * 100;
+		
+		for (v = s; v < s + 100; v++) {
+			if (v % F == 0) {
+				break;
 			}
 		}
-
-		return cnt;
+		
+		String str = String.valueOf(v);
+		System.out.println(str.substring(str.length() - 2, str.length()));
 	}
 }
