bd08be68895f3b2a402af00731b1a00a7a60709c
redggum <redggum@naver.com>
2018-12-14 01:27:32 +0900

    failed

--- src/Main.java
+++ src/Main.java
@@ -2,29 +2,31 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-/* 1107번 리모컨 */
+/* 1789번 수들의 합 */
 
 public class Main {
 
-	static int N; // 0 <= N <= 500,000
-	static int M; // 0 <= N <= 10
-	static int S = 100;
-	static boolean[] brokenBtn = new boolean[10];
-	static int cnt = 0;
+	static long S; // 1 ≤ S ≤ 4,294,967,295
+	static long sub = 1;
+	static long cnt = 0;
 
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		S = Long.parseLong(br.readLine());
 		
-		N = Integer.parseInt(br.readLine());
-		M = Integer.parseInt(br.readLine());
-		
-		String[] strs = br.readLine().split(" ");
-		
-		for (int i = 0; i < strs.length; i++) {
-			brokenBtn[Integer.parseInt(strs[i])] = true;
+		if (S == 1) {
+			System.out.println(1);
+			return;
+		}
+
+		while (S - sub > 0) {
+			S -= sub;
+			cnt++;
+			sub++;
 		}
 		
-		
+		System.out.println(cnt);
 	}
 }
