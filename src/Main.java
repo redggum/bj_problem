7cc3a88fd38d1578850d59490cee04a7174a2966
redggum <redggum@naver.com>
2018-12-16 01:00:37 +0900

    init

--- src/Main.java
+++ src/Main.java
@@ -2,42 +2,21 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-/* 1357번 뒤집힌 덧셈 */
+/* 1964번 오각혁,오각형,오각형... */
 
 public class Main {
 
-	static int X, Y;	// X, Y <= 1,000
-	static long DIV = 15746;
-	static long sum = 0;
-	static long[] D;
+	static long N;	// 1 <= N <= 10,000,000
 
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String[] strs = br.readLine().split(" ");
 		
-		X = Integer.parseInt(strs[0]);
-		Y = Integer.parseInt(strs[1]);
+		A = Long.parseLong(strs[0]);
+		B = Long.parseLong(strs[1]);
+		C = Long.parseLong(strs[2]);
 		
-		System.out.println(Rev(Rev(X) + Rev(Y)));
-	}
-	
-	static int Rev(int frd) {
-		String[] strs = String.valueOf(frd).split("");
-		String tmp;
-		String ret = "";
-		int len = strs.length;
-		for (int i = 0; i < len / 2; i++) {
-			tmp = strs[i];
-			strs[i] = strs[len - 1 - i];
-			strs[len - 1 - i] = tmp;
-		}
-
-		for (int i = 0; i < len; i++) {
-			ret += strs[i];
-		}
 		
-		return Integer.parseInt(ret);
 	}
-	
 }
