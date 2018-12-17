8a6b94dfdd0d0570d3690585a488484a0a6b56f3
redggum <redggum@naver.com>
2018-12-17 23:56:00 +0900

    init

--- src/Main.java
+++ src/Main.java
@@ -18,28 +18,25 @@ public class Main {
 		A = Long.parseLong(strs[0]);
 		B = Long.parseLong(strs[1]);
 		
-//		in / N = in % N
 		
-		// in 결과
-		
-		// 몫 X, 나머지 X
-		// X 는 N보다 작음
-		// X * N + X = in -> X * (N + 1) = in;
-		// X -> 1 ~ 1,999,999 for문
-		// 곱하기 N 값 중 나머지 N이 X로 되는 수
-		
-		// e.g.) N = 3;
-		// i = 1, 2
-		// i = 1 -> in = 3; 1 * (3 + 1) = 4;
-		// i = 2 -> in = 6; 2 * (3 + 1) = 8;
-		
-		for (int i = 1; i < N; i++) {
-			in = i * (N + 1);
-//			System.out.println(in);
-			sum += in;
+		// guess B >= A
+		if (A > B) {
+			long tmp = A;
+			A = B;
+			B = tmp;
 		}
 		
-		System.out.println(sum);
+//		if (A >= 0 && B >= 0) {
+			
+			sum = (B * (B + 1) / 2) - (A * (A - 1) / 2);
+			
+//		} else if (A < 0 && B >= 0) {
+//			sum = (B * (B + 1) / 2) + (A * (A - 1) / 2);
+//			
+//		} else {	// A < 0 && B < 0
+//			sum = (A * (A - 1) / 2) + (B * (B + 1) / 2);
+//		}
 		
+		System.out.println(sum);
 	}
 }
