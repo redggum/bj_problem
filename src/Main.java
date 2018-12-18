c81b64a964dfd3d454cac52c1fad988e1afb9fd1
redggum <redggum@naver.com>
2018-12-19 02:40:19 +0900

    init

--- src/Main.java
+++ src/Main.java
@@ -2,34 +2,27 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-/* 1297번 TV 크기 */
+/* 1350번 진짜 공간 */
 
 public class Main {
 
-	static int A, B, C = 0; // 5 <= A < 1000, 1 <= B <= 99, 2 <= C <= 100, C > B
-	static int H, W = 0;
+	static int N = 0; // nums of files -> N <= 1000
+	static int[] S;	// size of each file, S <= 1,000,000,000
+	static int C; // size of cluster, C <= 1,048,576
 
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+		N = Integer.parseInt(br.readLine());
 		String[] strs = br.readLine().split(" ");
-
-		A = Integer.parseInt(strs[0]); // 대각선
-		B = Integer.parseInt(strs[1]); // 높이 비율
-		C = Integer.parseInt(strs[2]); // 너비 비율
-
-		// H^2 + W^2 = A^2
-		// B : C = H : W -> C * H = B * W
-		// H = Bx, W = Cx
-		// Bx^2 + Cx^2 = A^2
-		// B^2 * x^2 + C^2 * x^2 = A^2
-		// (B^2 + C^2) * x^2 = A^2
-
-		double tmp = Math.pow((double) A, 2) / (Math.pow((double) B, 2) + Math.pow((double) C, 2));
-
-		tmp = Math.sqrt(tmp);
+		S = new int[N + 1];
 		
-		System.out.println((int) (B * tmp) + " " + (int) (C * tmp));
+		for (int i = 0; i < N; i++) {
+			S[i + 1] = Integer.parseInt(strs[i]); // 대각선
+		}
+		
+		C = Integer.parseInt(br.readLine());
+
 	}
 }
