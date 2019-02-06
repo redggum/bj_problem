5fe45d8448f8c644ca95a962fa987fabd059b801
redggum <redggum@naver.com>
2019-02-07 00:52:48 +0900

    success

--- src/Main.java
+++ src/Main.java
@@ -1,48 +1,41 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
-/* 9550번 아이들은 사탕을 좋아해 */
+/* 1712번 손익분기점 */
 
 public class Main {
-
-	static int T = 0; // 1 <= T <= 100
-	static int N, K = 0; // 1 <= N, K <= 100
-	static int[] arr;
-	static int[] comp;
-	static int[] sol;
-	static int sol_idx = 0;
-	static boolean[] visited;
-	static int order = 0;
-	static int candy[];
-	static int sum;
 
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String[] strs;
+		long A, B, C, tmpA, tmpB;
 
-		T = Integer.parseInt(br.readLine());
+		strs = br.readLine().split(" ");
 
+		A = Long.parseLong(strs[0]);
+		B = Long.parseLong(strs[1]);
+		C = Long.parseLong(strs[2]);
 
-		for (int tc = 1; tc <= T; tc++) {
-			strs = br.readLine().split(" ");
-			
-			N = Integer.parseInt(strs[0]);
-			K = Integer.parseInt(strs[1]);
+		// A + Bn < Cn
+		// A < n (C - B)
+		// A / (C - B) < n
+		// A, B, C 는 21억 이하의 자연수
 
-			candy = new int[N];
-			sum = 0;
-			
-			strs = br.readLine().split(" ");
-			
-			for (int i = 0; i < strs.length; i++) {
-				candy[i] = Integer.parseInt(strs[i]);
-				sum += (candy[i] / K);
+		tmpA = (C - B);
+
+		if (tmpA == 0) {
+			System.out.println(-1);
+		} else {
+			tmpB = A / tmpA;
+
+			if (tmpB < 0) {
+				System.out.println(-1);
+			} else {
+				System.out.println(tmpB + 1);
 			}
-			
-			System.out.println(sum);
 		}
+
 	}
 }
