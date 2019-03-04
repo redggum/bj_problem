bfe90cb3c5238f95ee2a4a389ea3a7e1f6d644b7
redggum <redggum@naver.com>
2019-03-05 00:13:18 +0900

    success

--- src/Main.java
+++ src/Main.java
@@ -4,30 +4,49 @@ import java.io.InputStreamReader;
 import java.sql.Array;
 import java.util.Arrays;
 
-/* 2675번 문자열 반복 */
+/* 1157번 단어 공부 */
 
 public class Main {
 
 	static String S;
 	static String[] strs;
-	static int T;
-	static int cnt;
+	static int[] freqAlphabet = new int[26];	// Upper, lower diff = 32
+	static int ascii;
+	static char max;
+	static int maxAlphabetIndex = -1;
+	static int maxCnt = 0;
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		S = br.readLine();
 		
-		T = Integer.parseInt(br.readLine());
+		for (int i = 0; i < S.length(); i++) {
+			ascii = (int) (S.charAt(i) - 'A');
+			
+			if (ascii >= 32) {
+				ascii -= 32;
+			}
+			
+			freqAlphabet[ascii]++;
+//			System.out.println("ascii : " + ascii);
+		}
 		
-		for (int tc = 1; tc <= T; tc++) {
-			strs = br.readLine().split(" ");
-			
-			cnt = Integer.parseInt(strs[0]);
-			S = strs[1];
-			
-			for (int i = 0; i < S.length(); i++) {
-				for (int j = 0; j < cnt; j++) {
-					System.out.print(S.charAt(i));
-				}
+		for (int i = 0; i < freqAlphabet.length; i++) {
+			if (freqAlphabet[i] > 0 && freqAlphabet[i] > maxCnt) {
+				maxCnt = freqAlphabet[i];
+				max = (char) (i + (int) 'A');
+				maxAlphabetIndex = i;
+				
+//				System.out.println("maxAlphabetIndex : " + maxAlphabetIndex);
 			}
 		}
+		
+		for (int i = 0; i < freqAlphabet.length; i++) {
+			if (maxCnt == freqAlphabet[i] && maxAlphabetIndex != i) {
+				max = '?';
+				break;
+			}
+		}
+		
+		System.out.println(max);
 	}
 }
