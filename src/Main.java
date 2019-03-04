3c17a908a97746cb41024556759bbbbda566c64e
redggum <redggum@naver.com>
2019-03-04 23:28:33 +0900

    success

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,33 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.sql.Array;
+import java.util.Arrays;
 
-/* 11654번 아스키 코드 */
+/* 10809번 알파벳 찾기 */
 
 public class Main {
 
-	static char in;
+	static int[] pos = new int[26];
+	static int index = -1;
+	static String S;
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		in = br.readLine().charAt(0);
+		Arrays.fill(pos, -1);
+		
+		S = br.readLine();
 
-		System.out.println((int) in);
+		for (int i = 0; i < S.length(); i++) {
+			index = S.charAt(i) - 'a';
+			
+			if (pos[index] == -1) {
+				pos[index] = i;
+			}
+		}
+		
+		for (int i = 0; i < pos.length; i++) {
+			System.out.print(pos[i] + " ");
+		}
 	}
 }
