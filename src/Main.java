ad1a08bbc7808368c7198c33b97b784eeee5c5de
redggum <redggum@naver.com>
2019-03-04 23:12:53 +0900

    success

--- src/Main.java
+++ src/Main.java
@@ -2,29 +2,16 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-/* 1267번  */
+/* 11654번 아스키 코드 */
 
 public class Main {
 
-	static int N, sum, call = 0;
-	static int DIV_Y = 30;
-	static int DIV_M = 60;
-	static int PRC_Y = 10;
-	static int PRC_M = 15;
-
-	public static void main(String[] args) throws IOException {
-
+	static char in;
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String[] strs;
 		
-		N = Integer.parseInt(br.readLine());
-		
-		strs = br.readLine().split(" ");
+		in = br.readLine().charAt(0);
 
-		for (int i = 0; i < N; i++) {
-			call = Integer.parseInt(strs[0]);
-			
-			
-		}
-	}		
+		System.out.println((int) in);
+	}
 }
