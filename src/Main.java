be425a8db79419f360a55eca1340168cf16ad9dc
redggum <redggum@naver.com>
2019-03-05 23:06:49 +0900

    success

--- src/Main.java
+++ src/Main.java
@@ -1,56 +1,35 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.sql.Array;
-import java.util.Arrays;
+import java.util.Scanner;
 
-/* 1316번 그룹 단어 체크 */
+/* 1152번 단어의 개수 */
 
 public class Main {
 
-	static String S;
 	static String[] strs;
-	static int[] alphabet;
-	static int T;
-	static int total = 0;
-	static boolean failed;
+	static int len = 0;
+	static boolean trigger = false;
+
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		T = Integer.parseInt(br.readLine());
+		String s = br.readLine();
+		String c;
+		int cnt = 0;
 
-		int index = 0;
-		
-		for (int tc = 1; tc <= T; tc++) {
-			strs = br.readLine().split("");
+		for (int i = 0; i < s.length(); i++) {
+			c = String.valueOf(s.charAt(i));
 			
-			failed = false;
-			
-			total++;
-			alphabet = new int[26];
-			
-			for (int i = 0; i < strs.length; i++) {
-//				System.out.println("word : " + strs[i]);
-				index = strs[i].charAt(0) - 'a';
-				
-//				System.out.println("alphabet[" + index + "] : " + alphabet[index]);
-				
-				if (alphabet[index] == 0 || i == 0) {
-					alphabet[index] = 1;
-					continue;
-				} else {
-//					System.out.println("strs[i - 1].charAt(0) - 'a' : " + (strs[i - 1].charAt(0) - 'a'));
-					if (strs[i - 1].charAt(0) - 'a' == index) {
-						continue;
-					} else {
-						total--;
-						break;
-					}
+			if (c.equals(" ")) {
+				trigger = false;
+			} else {
+				if (trigger == false) {
+					cnt++;
+					trigger = true;
 				}
 			}
-			
-//			System.out.println("total : " + total);
 		}
 		
-		System.out.println(total);
+		System.out.println(cnt);
 	}
 }
