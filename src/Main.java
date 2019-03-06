d1991da9559dcdbbc54d26a809afb8b6231c82d3
redggum <redggum@naver.com>
2019-03-06 23:59:47 +0900

    success

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,24 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-/* 7287번 등록 */
+/* 2441번 별 찍기 - 4 */
 
 public class Main {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
-//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		System.out.println(103);
-		System.out.println("redggum");
+		int num = Integer.parseInt(br.readLine());
+		
+		for (int i = 1; i <= num; i++) {
+			String str = "";
+			for (int j = 1; j < i; j++) {
+				str += " ";
+			}
+			for (int j = num; j >= i ; j--) {
+				str += "*";
+			}
+			System.out.println(str);
+		}
 	}
 }
