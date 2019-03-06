c917e41b263a378b44897b6319c33b32f66c0132
redggum <redggum@naver.com>
2019-03-07 00:07:11 +0900

    success

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,20 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-/* 2558번 A + B - 2 */
+/* 8393번 합 */
 
 public class Main {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		int a = Integer.parseInt(br.readLine());
-		int b = Integer.parseInt(br.readLine());
+		int num = Integer.parseInt(br.readLine());
+		int sum = 0;
 
-		System.out.println(a + b);
+		for (int i = 1; i <= num; i++) {
+			sum += i;
+		}
+		
+		System.out.println(sum);
 	}
 }
