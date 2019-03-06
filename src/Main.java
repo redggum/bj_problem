95cf6c24a983d6332e60d1650b04864f1180f66f
redggum <redggum@naver.com>
2019-03-06 22:29:27 +0900

    success

--- src/Main.java
+++ src/Main.java
@@ -2,33 +2,34 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-/* 1152번 단어의 개수 */
+/* 2908번 상수 */
 
 public class Main {
 
 	static String[] strs;
+	static String a, b;
 	static int len = 0;
 	static boolean trigger = false;
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String s = br.readLine();
-		String c;
-		int cnt = 0;
-
-		for (int i = 0; i < s.length(); i++) {
-			c = String.valueOf(s.charAt(i));
-			
-			if (c.equals(" ")) {
-				trigger = false;
+		strs = br.readLine().split(" ");
+		
+		a = strs[0].charAt(2) + "" + strs[0].charAt(1) + "" + strs[0].charAt(0);
+		b = strs[1].charAt(2) + "" + strs[1].charAt(1) + "" + strs[1].charAt(0);
+		
+		
+		
+		for (int i = 0; i < 3; i++) {
+			if (a.charAt(i) > b.charAt(i)) {
+				System.out.println(a);
+				break;
+			} else if (a.charAt(i) < b.charAt(i)) {
+				System.out.println(b);
+				break;
 			} else {
-				if (trigger == false) {
-					cnt++;
-					trigger = true;
-				}
+				continue;
 			}
 		}
-		
-		System.out.println(cnt);
 	}
 }
