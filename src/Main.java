ea281028301c235743572874e682947b26370b13
redggum <redggum@naver.com>
2019-03-06 23:06:03 +0900

    success

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,18 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
-/* 10718번 We love kriii */
+/* 2742번 기찍 N */
 
 public class Main {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
-		System.out.println("강한친구 대한육군");
-		System.out.println("강한친구 대한육군");
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int num = Integer.parseInt(br.readLine());
+		
+		for (int i = num; i > 0; i--) {
+			System.out.println(i);
+		}
 	}
 }
