8bf990c5cc1b81e641b90b1f787c939b116224d7
redggum <redggum@naver.com>
2019-03-06 23:01:17 +0900

    success

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,11 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
-/* 2739번 구구단 */
+/* 10718번 We love kriii */
 
 public class Main {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-		int num = Integer.parseInt(br.readLine());
-		
-		for (int i = 1; i <= 9; i++) {
-			System.out.println(num + " * " + i + " = " + (num * i));
-		}
+		System.out.println("강한친구 대한육군");
+		System.out.println("강한친구 대한육군");
 	}
 }
