edb7cb319ec2d445291add7eff01fe620e36910b
redggum <redggum@naver.com>
2019-03-07 00:05:19 +0900

    success

--- src/Main.java
+++ src/Main.java
@@ -2,25 +2,16 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-/* 9498번 시험 성적 */
+/* 2558번 A + B - 2 */
 
 public class Main {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		int num = Integer.parseInt(br.readLine());
+		int a = Integer.parseInt(br.readLine());
+		int b = Integer.parseInt(br.readLine());
 
-		if (num >= 90 && num <= 100) {
-			System.out.println("A");
-		} else if (num >= 80 && num <= 89) {
-			System.out.println("B");
-		} else if (num >= 70 && num <= 79) {
-			System.out.println("C");
-		} else if (num >= 60 && num <= 69) {
-			System.out.println("D");
-		} else {
-			System.out.println("F");
-		}
+		System.out.println(a + b);
 	}
 }
