0cbbad79fe10a2eeac0b53212cea33a36cc7b8d3
redggum <redggum@naver.com>
2019-03-07 23:22:05 +0900

    success

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 
-/* 1924번 2007년 */
+/* 10817번 세 수 */
 
 public class Main {
 
@@ -10,32 +14,18 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String[] strs = br.readLine().split(" ");
 		
-		int month = Integer.parseInt(strs[0]);
-		int day = Integer.parseInt(strs[1]);
+		int a = Integer.parseInt(strs[0]);
+		int b = Integer.parseInt(strs[1]);
+		int c = Integer.parseInt(strs[2]);
+
+		ArrayList<Integer> arr = new ArrayList<Integer>();
 		
-//		0 : Sun
-//		1 : Mon
-//		2 : Tue
-//		3 : Wed
-//		4 : Thu
-//		5 : Fri
-//		6 : Sat
-		String[] week = {"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"};
+		arr.add(a);
+		arr.add(b);
+		arr.add(c);
 		
-		int[] mons = new int[13];
-		mons[1] = 0;	// 31			% 7
-		mons[2] = 31;	// 28
-		mons[3] = 59;	// 31
-		mons[4] = 90;	// 30
-		mons[5] = 120;	// 31
-		mons[6] = 151;	// 30
-		mons[7] = 181;	// 31
-		mons[8] = 212;	// 31
-		mons[9] = 243;	// 30
-		mons[10] = 273;	// 31
-		mons[11] = 304;	// 30
-		mons[12] = 334;	// 31
+		Collections.sort(arr);
 		
-		System.out.println(week[(mons[month] + day) % 7]);
+		System.out.println(arr.get(1));
 	}
 }
