7b83c7a27bed6e56583a5da640f862b876f257ec
redggum <redggum@naver.com>
2019-03-08 00:12:08 +0900

    runtime error.. but I duno why...

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,35 @@
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
-/* 11718번 그대로 출력하기 */
+/* 10871번 X보다 작은 수 */
 
 public class Main {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 //		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		Scanner sc = new Scanner(System.in);
-
-		ArrayList<String> arr = new ArrayList<String>();
-
-		while(sc.hasNextLine()) {
-			String in = sc.nextLine();
-			
-			if (in.isEmpty() || in == null) {
-				break;
-			}
-			arr.add(in);
-		}
-		sc.close();
 		
-//		System.out.println(arr.size());
-		for (int i = 0; i < arr.size(); i++) {
-			System.out.println(arr.get(i));
+//		String[] strs = br.readLine().split(" ");
+		String[] strs = sc.nextLine().split(" ");
+		
+		int a = Integer.parseInt(strs[0]);
+		int x = Integer.parseInt(strs[1]);
+		
+		int[] arr = new int[10];
+		String str = "";
+		
+		strs = sc.nextLine().split(" ");
+		
+		for (int i = 0; i < a; i++) {
+			arr[i] = Integer.parseInt(strs[i]);
+			
+			if (arr[i] < x) {
+				str += arr[i] + " ";
+			}
 		}
+		
+		System.out.println(str);
 	}
 }
