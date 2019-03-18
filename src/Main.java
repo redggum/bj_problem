50bb17f202271ef6814d5fd9328d73869bf15047
redggum <redggum@naver.com>
2019-03-18 23:55:14 +0900

    success

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,27 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
 
-/* 1546번 평균 */
+/* 10039번 평균 점수 */
 
 public class Main {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int[] arr = new int[5];
+		int sum = 0;
 		
-		int cnt = Integer.parseInt(br.readLine());
-		String[] strs = br.readLine().split(" ");
-		
-		ArrayList<Integer> arr = new ArrayList<Integer>();
-		
-		for (int i = 0; i < cnt; i++) {
-			arr.add(Integer.parseInt(strs[i]));
+		for (int i = 0; i < 5; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+			
+			if (arr[i] <= 40) {
+				arr[i] = 40;
+			}
+			
+			sum += arr[i];
 		}
 		
-		Collections.sort(arr);
-		
-		double max = (double) arr.get(cnt - 1);
-		double sum = 0;
-		
-//		System.out.println("max : " + max);
-		
-		for (int i = 0; i < cnt; i++) {
-			sum += (arr.get(i) / max * 100);
-		}
-		
-		System.out.println(sum / arr.size());
+		System.out.println(sum / 5);
 	}
 }
