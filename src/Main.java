30ed0b54d8e2d279958a3ac623f1353a95a7b3bd
redggum <redggum@naver.com>
2019-03-18 23:24:46 +0900

    success

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,34 @@
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Scanner;
+import java.io.InputStreamReader;
 
-/* 11719번 그대로 출력하기 2 */
+/* 1110번 더하기 사이클 */
 
 public class Main {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
-		Scanner sc = new Scanner(System.in);
-        while(sc.hasNextLine()) {
-            String input = sc.nextLine();
-            System.out.println(input);
-        }
-        sc.close();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int num = Integer.parseInt(br.readLine());
+		
+		int a = (num >= 10 ? num / 10 : 0);
+		int b = num % 10;
+		int c = 0;
+		
+		int cnt = 0;
+		
+		while (true) {
+			c = (b * 10) + (a + b) % 10;
+//			System.out.println("a : " + a + ", b : " + b + ", c : " + c);
+			cnt++;
+			if (c == num) {
+				break;
+			}
+			
+			a = (c >= 10 ? c / 10 : 0);
+			b = c % 10;
+		}
+		
+		System.out.println(cnt);
 	}
 }
