464b11732e3a577c3eb0ed540d8a54df0cf1e66d
redggum <redggum@naver.com>
2019-03-18 23:51:25 +0900

    success

--- src/Main.java
+++ src/Main.java
@@ -4,24 +4,33 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
 
-/* 2750번 수 정렬하기 */
+/* 1546번 평균 */
 
 public class Main {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
 		int cnt = Integer.parseInt(br.readLine());
+		String[] strs = br.readLine().split(" ");
 		
 		ArrayList<Integer> arr = new ArrayList<Integer>();
 		
 		for (int i = 0; i < cnt; i++) {
-			arr.add(Integer.parseInt(br.readLine()));
+			arr.add(Integer.parseInt(strs[i]));
 		}
 		
 		Collections.sort(arr);
 		
+		double max = (double) arr.get(cnt - 1);
+		double sum = 0;
+		
+//		System.out.println("max : " + max);
+		
 		for (int i = 0; i < cnt; i++) {
-			System.out.println(arr.get(i));
+			sum += (arr.get(i) / max * 100);
 		}
+		
+		System.out.println(sum / arr.size());
 	}
 }
