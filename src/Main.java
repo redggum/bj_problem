25de522b6aa2350d758b2a26954f56d2ce17d4b9
redggum <redggum@naver.com>
2019-03-18 22:32:52 +0900

    success

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,16 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
-/* 11720번 숫자의 합 */
+/* 11719번 그대로 출력하기 2 */
 
 public class Main {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-		StringBuffer sb = new StringBuffer();
-		sb.append(br.readLine());
-		
-		int len = 0;
-		
-		for (int i = 0; i < sb.length(); i++) {
-//			System.out.println("len : " + len);
-			
-			if (len + 10 >= sb.length()) {
-				System.out.println(sb.substring(len, sb.length()));
-				break;
-			}
-			
-			System.out.println(sb.substring(len, len + 10));
-			
-			
-			len += 10;
-			i += 9;
-		}
-		
+		Scanner sc = new Scanner(System.in);
+        while(sc.hasNextLine()) {
+            String input = sc.nextLine();
+            System.out.println(input);
+        }
+        sc.close();
 	}
 }
