8b6ecf1e7bd43066838a9b2edb62341262ef3d41
redggum <redggum@naver.com>
2019-03-19 23:08:49 +0900

    success

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,46 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
-/* 10039번 평균 점수 */
+/* 4344번 평균은 넘겠지 */
 
 public class Main {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		int[] arr = new int[5];
-		int sum = 0;
-		
-		for (int i = 0; i < 5; i++) {
-			arr[i] = Integer.parseInt(br.readLine());
-			
-			if (arr[i] <= 40) {
-				arr[i] = 40;
+		int T = Integer.parseInt(br.readLine());
+		String[] strs;
+		int N;
+		ArrayList<Integer> arr;
+
+		for (int t = 1; t <= T; t++) {
+			strs = br.readLine().split(" ");
+			N = Integer.parseInt(strs[0]);
+
+			arr = new ArrayList<Integer>();
+			int sum = 0;
+
+			for (int i = 0; i < N; i++) {
+				arr.add(Integer.parseInt(strs[i + 1]));
+				sum += arr.get(i);
 			}
-			
-			sum += arr[i];
+
+			double avr = sum / N;
+			double cnt = 0;
+//			System.out.println("avr : " + avr);
+
+			for (int i = 0; i < N; i++) {
+				if (avr < arr.get(i)) {
+					cnt++;
+				}
+			}
+
+//			System.out.println("cnt : " + cnt);
+
+			System.out.printf("%.3f", Math.round(cnt / N * 100000) / 1000.0);
+			System.out.println("%");
 		}
-		
-		System.out.println(sum / 5);
 	}
 }
