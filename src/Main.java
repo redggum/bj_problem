bcc6d2d7698a88d3686563a377d17be4772720d3
redggum <redggum@naver.com>
2019-03-26 00:33:43 +0900

    success

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,64 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
-/* 2747번 피보나치 수 */
+/* 10845번 큐 */
 
 public class Main {
+	
+	static List<Integer> arr = new ArrayList<Integer>();
 	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		int N = Integer.parseInt(br.readLine());
+		String[] strs;
+		String cmd;
+		int param = 0;
 		
-		// n = 1 -> 1
-		// n = 2 -> 2
-		// n = 3 -> n(2) + n(1)
-		// n = 4 -> n(3) + n(2)
-		// n = 5 -> n(4) + n(3)
-		// n = 6 -> n(5) + n(4)
-		// n = n -> n(n - 1) + n(n - 2)
-		
-		int[] sum = new int[N + 1];
-		
-		sum[0] = 0;
-		sum[1] = 1;
-		
-		for (int n = 2; n <= N; n++) {
-			sum[n] = sum[n - 2] + sum[n - 1];
+		for (int i = 0; i < N; i++) {
+			strs = br.readLine().split(" ");
+			if (strs.length > 1) {
+				param = Integer.parseInt(strs[1]);
+			}
+			
+			cmd = strs[0];
+			
+			queue(cmd, param);
 		}
-
-		System.out.println(sum[N]);
+	}
+	
+	static void queue(String command, int param) {
+//		System.out.println("cmd : " + command);
+		
+		if (command.equals("push")) {
+			arr.add(param);
+		} else if (command.equals("pop")) {
+			if (arr.size() == 0) {
+				System.out.println(-1);
+			} else {
+				System.out.println(arr.get(0));
+				arr.remove(arr.get(0));
+			};
+		} else if (command.equals("size")) {
+			System.out.println(arr.size());
+		} else if (command.equals("empty")) {
+			System.out.println(arr.size() == 0 ? 1 : 0);
+		} else if (command.equals("front")) {
+			if (arr.size() == 0) {
+				System.out.println(-1);
+			} else {
+				System.out.println(arr.get(0));
+			};
+		} else if (command.equals("back")) {
+			if (arr.size() == 0) {
+				System.out.println(-1);
+			} else {
+				System.out.println(arr.get(arr.size() - 1));
+			};
+		}
 	}
 
 }
