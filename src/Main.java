dae8125847895ff69e28724dfe5e3d9113283f9a
redggum <redggum@naver.com>
2019-03-26 22:39:27 +0900

    success

--- src/Main.java
+++ src/Main.java
@@ -1,64 +1,26 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 
-/* 10845번 큐 */
+/* 10950번 A + B - 3 */
 
 public class Main {
-	
-	static List<Integer> arr = new ArrayList<Integer>();
 	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		int N = Integer.parseInt(br.readLine());
 		String[] strs;
-		String cmd;
-		int param = 0;
+		int a;
+		int b;
 		
 		for (int i = 0; i < N; i++) {
 			strs = br.readLine().split(" ");
-			if (strs.length > 1) {
-				param = Integer.parseInt(strs[1]);
-			}
 			
-			cmd = strs[0];
+			a = Integer.parseInt(strs[0]);
+			b = Integer.parseInt(strs[1]);
 			
-			queue(cmd, param);
+			System.out.println((a + b));
 		}
 	}
-	
-	static void queue(String command, int param) {
-//		System.out.println("cmd : " + command);
-		
-		if (command.equals("push")) {
-			arr.add(param);
-		} else if (command.equals("pop")) {
-			if (arr.size() == 0) {
-				System.out.println(-1);
-			} else {
-				System.out.println(arr.get(0));
-				arr.remove(arr.get(0));
-			};
-		} else if (command.equals("size")) {
-			System.out.println(arr.size());
-		} else if (command.equals("empty")) {
-			System.out.println(arr.size() == 0 ? 1 : 0);
-		} else if (command.equals("front")) {
-			if (arr.size() == 0) {
-				System.out.println(-1);
-			} else {
-				System.out.println(arr.get(0));
-			};
-		} else if (command.equals("back")) {
-			if (arr.size() == 0) {
-				System.out.println(-1);
-			} else {
-				System.out.println(arr.get(arr.size() - 1));
-			};
-		}
-	}
-
 }
