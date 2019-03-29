46da1cc16d63383bfe7c7d59b97dc5ecb975b5d6
redggum <redggum@naver.com>
2019-03-30 00:27:32 +0900

    success

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,67 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 
-/* 2292번 벌집 */
+/* 1697번 숨바꼭질 */
 
 public class Main {
 	static int MUL = 6;
+	static int[] visited = new int[200000 + 1];
+	static int N, K;
+	static int steps = 0;
+	static int result = Integer.MAX_VALUE;
+	static Queue<Integer> q = new LinkedList<Integer>();
+	static int level = 0;
+	static double sum = 0;
+	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		long N = Long.parseLong(br.readLine());
-		long sum = 1;
+		String[] strs = br.readLine().split(" ");
+		int node;
+		int next;
 		
-		// 1,(6) 7, (12) 19, (18) 37, (24) 61
-		for (int i = 1; i <= N; i++) {
-			sum += (MUL * (i - 1));
+		N = Integer.parseInt(strs[0]);
+		K = Integer.parseInt(strs[1]);
+	
+		q.offer(N);
+		visited[N] = 1;
+		
+		
+		while(!q.isEmpty()) {
+			node = q.poll();
+//			System.out.println("node : " + node);
 			
-			if (N <= sum) {
-				System.out.println(i);
-				break;
+			if (node == K) {
+				System.out.println(visited[node] - 1);
+				return;
 			}
+			
+			for (int i = 0; i < 3; i++) {
+				next = nextNode(node, i);
+				
+				if (next > visited.length - 1) {
+					continue;
+				}
+				
+				if (next >= 0 && visited[next] == 0) {
+					q.offer(next);
+					visited[next] = visited[node] + 1;
+				}
+			}
+		}
+	}
+	
+	static int nextNode(int node, int index) {
+//		System.out.println("node : " + node + ", index : " + index);
+		if (index == 0) {
+			return node - 1;
+		} else if (index == 1) {
+			return node + 1;
+		} else {
+			return node * 2;
 		}
 	}
 }
