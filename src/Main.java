0927721ca6888229507c6995ba3ad8615ded5d6b
redggum <redggum@naver.com>
2019-03-30 00:37:01 +0900

    success

--- src/Main.java
+++ src/Main.java
@@ -1,67 +1,31 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
-/* 1697번 숨바꼭질 */
+/* 15552번 빠른 A + B */
 
 public class Main {
-	static int MUL = 6;
-	static int[] visited = new int[200000 + 1];
-	static int N, K;
-	static int steps = 0;
-	static int result = Integer.MAX_VALUE;
-	static Queue<Integer> q = new LinkedList<Integer>();
-	static int level = 0;
-	static double sum = 0;
-	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int T = Integer.parseInt(br.readLine());
+		String[] strs;
+		int a, b;
+		StringTokenizer st;
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
-		String[] strs = br.readLine().split(" ");
-		int node;
-		int next;
-		
-		N = Integer.parseInt(strs[0]);
-		K = Integer.parseInt(strs[1]);
-	
-		q.offer(N);
-		visited[N] = 1;
-		
-		
-		while(!q.isEmpty()) {
-			node = q.poll();
-//			System.out.println("node : " + node);
+		for (int tc = 1; tc <= T; tc++) {
+//			strs = br.readLine().split(" ");
+			st = new StringTokenizer(br.readLine(), " ");
+			a = Integer.parseInt(st.nextToken());
+			b = Integer.parseInt(st.nextToken());
 			
-			if (node == K) {
-				System.out.println(visited[node] - 1);
-				return;
-			}
-			
-			for (int i = 0; i < 3; i++) {
-				next = nextNode(node, i);
-				
-				if (next > visited.length - 1) {
-					continue;
-				}
-				
-				if (next >= 0 && visited[next] == 0) {
-					q.offer(next);
-					visited[next] = visited[node] + 1;
-				}
-			}
+			bw.write(a + b + "\n");
 		}
-	}
-	
-	static int nextNode(int node, int index) {
-//		System.out.println("node : " + node + ", index : " + index);
-		if (index == 0) {
-			return node - 1;
-		} else if (index == 1) {
-			return node + 1;
-		} else {
-			return node * 2;
-		}
+		
+		bw.flush();
 	}
 }
