116ca85275c687d63119ba01c0dfd98fc6bc0009
redggum <redggum@naver.com>
2019-04-02 00:38:21 +0900

    success

--- src/Main.java
+++ src/Main.java
@@ -1,50 +1,41 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
-/* 2156번 포도주 시식 */
+/* 2309번 일곱 난쟁이 */
 
 public class Main {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-		int N = Integer.parseInt(br.readLine());
 		
-		int[] val = new int[N + 1];
-		int[][] d = new int[N + 1][2];
-
-		val[1] = Integer.parseInt(br.readLine());
-		val[2] = Integer.parseInt(br.readLine());
+		int[] dwarf = new int[9];
+		int[] noDwarf = new int[2];
+		int sum = 0;
 		
-		d[1][0] = 0;
-		d[1][1] = val[1];
-		
-		d[2][0] = d[1][1];
-		d[2][1] = max(val[2] + d[1][1], val[2] + d[1][0]);
-		
-//		System.out.println("d[" + 1 + "][0] : " + d[1][0]);
-//		System.out.println("d[" + 1 + "][1] : " + d[1][1]);
-//		System.out.println("d[" + 2 + "][0] : " + d[2][0]);
-//		System.out.println("d[" + 2 + "][1] : " + d[2][1]);
-		
-		for (int i = 3; i <= N; i++) {
-			val[i] = Integer.parseInt(br.readLine());
-			
-			d[i][0] = max(d[i - 1][1], d[i - 1][0]);
-			d[i][1] = d[i -1][1] > d[i - 2][1] ? max(val[i] + val[i - 1] + d[i - 2][0], val[i] + d[i - 2][1]) : val[i] + d[i - 1][0];
-			
-//			System.out.println("d[" + i + "][0] : " + d[i][0]);
-//			System.out.println("d[" + i + "][1] : " + d[i][1]);
+		for (int i = 0; i < dwarf.length; i++) {
+			dwarf[i] = Integer.parseInt(br.readLine());
+			sum += dwarf[i];
 		}
 		
-		System.out.println(max(d[N][0], d[N][1]));
-	}
-	
-	public static int max(int a, int b) {
-		if (a > b) {
-			return a;
-		} else {
-			return b;
+		for (int i = 0; i < dwarf.length; i++) {
+			for (int j = i + 1; j < dwarf.length; j++) {
+				if (sum - dwarf[i] - dwarf[j] == 100) {
+					noDwarf[0] = dwarf[i];
+					noDwarf[1] = dwarf[j];
+				}
+			}
 		}
+		
+		Arrays.sort(dwarf);
+		
+		for (int i = 0; i < dwarf.length; i++) {
+			if (dwarf[i] == noDwarf[0] || dwarf[i] == noDwarf[1]) {
+				continue;
+			}
+			
+			System.out.println(dwarf[i]);
+		}
+		
 	}
 }
