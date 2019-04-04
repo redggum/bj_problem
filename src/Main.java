af395758700ecd6b0b78a30e2a79ba311b89cda6
redggum <redggum@naver.com>
2019-04-04 23:17:47 +0900

    success

--- src/Main.java
+++ src/Main.java
@@ -11,61 +11,27 @@ import java.util.Queue;
 import java.util.Set;
 import java.util.StringTokenizer;
 
-/* 2606번 바이러스 */
+/* 10952번 A+B - 5 */
 
 public class Main {
 	static int N, M, a, b;
-	
+
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		ArrayList<ArrayList<Integer>> arr;
 
-		N = Integer.parseInt(br.readLine());
-		M = Integer.parseInt(br.readLine());
 		String[] strs;
-		Queue<Integer> q = new LinkedList<Integer>();
-		boolean[] visited = new boolean[N + 1];
-		
-		arr = new ArrayList<ArrayList<Integer>>();
-		
-		for (int i = 0; i <= N; i++) {
-			arr.add(new ArrayList<Integer>());
-		}
-		
-		for (int i = 0; i < M; i++) {
+
+		while (true) {
 			strs = br.readLine().split(" ");
-			
+
 			a = Integer.parseInt(strs[0]);
 			b = Integer.parseInt(strs[1]);
 			
-			arr.get(a).add(b);
-			arr.get(b).add(a);
-		}
-		
-		q.offer(1);
-		visited[1] = true;
-		
-		int node;
-		int next;
-		int cnt = -1;
-		
-		while(!q.isEmpty()) {
-			node = q.poll();
-			cnt++;
-			
-			for (int i = 0; i < arr.get(node).size(); i++) {
-				next = arr.get(node).get(i);
-				
-				if (visited[next] == false) {
-					q.offer(next);
-					visited[next] = true;
-				}
+			if (a == 0 && b == 0) {
+				break;
 			}
+			
+			System.out.println(a + b);
 		}
-		
-		System.out.println(cnt);
-		
-		
 	}
 }
