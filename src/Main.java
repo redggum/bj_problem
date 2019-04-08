ccc34204859dffa635a508b63919bffed492ae79
redggum <redggum@naver.com>
2019-04-08 23:39:30 +0900

    success

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,42 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 
-/* 1237번 정ㅋ벅ㅋ */
+/* 11399번 ATM */
 
 public class Main {
-	static int T, M, N, K;
-	static ArrayList<Integer> arr;
-	static int[] parent;
+	static int N;
+	static int[] time;
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String[] strs;
 
-		System.out.println("문제의 정답");
+		N = Integer.parseInt(br.readLine());
+		strs = br.readLine().split(" ");
+		
+		time = new int[N];
+		
+		for (int i = 0; i < N; i++) {
+			time[i] = Integer.parseInt(strs[i]);
+		}
+		
+		Arrays.sort(time);
+		
+		int[] sum = new int[N];
+		sum[0] = time[0];
+		int total = sum[0];
+		
+		for (int i = 1; i < N; i++) {
+			sum[i] = sum[i - 1] + time[i];
+			
+//			System.out.println(time[i] + " " + sum[i]);
+			
+			total += sum[i];
+			
+		}
+		
+		System.out.println(total);
 	}
 }
