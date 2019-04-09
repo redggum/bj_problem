2cd0c2be712e34f2fc6940408312b9039807e9e1
redggum <redggum@naver.com>
2019-04-09 22:46:44 +0900

    success

--- src/Main.java
+++ src/Main.java
@@ -1,43 +1,24 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Scanner;
 
-/* 11399번 ATM */
+/* 10951번 A+B - 4 */
 
 public class Main {
 	static int N;
-	static int[] time;
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String[] strs;
-
-		N = Integer.parseInt(br.readLine());
-		strs = br.readLine().split(" ");
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		time = new int[N];
+		Scanner sc = new Scanner(System.in);
+		int a, b;
 		
-		for (int i = 0; i < N; i++) {
-			time[i] = Integer.parseInt(strs[i]);
+		while(sc.hasNextInt()) {
+			a = sc.nextInt();
+			b = sc.nextInt();
+			
+			System.out.println(a + b);
 		}
-		
-		Arrays.sort(time);
-		
-		int[] sum = new int[N];
-		sum[0] = time[0];
-		int total = sum[0];
-		
-		for (int i = 1; i < N; i++) {
-			sum[i] = sum[i - 1] + time[i];
-			
-//			System.out.println(time[i] + " " + sum[i]);
-			
-			total += sum[i];
-			
-		}
-		
-		System.out.println(total);
 	}
 }
