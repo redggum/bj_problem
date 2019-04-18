2fb6e9ad44545ccd9abac8f21c9c662966cf05d9
redggum <redggum@naver.com>
2019-04-18 23:58:40 +0900

    success

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,55 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.Scanner;
 
-/* 10951번 A+B - 4 */
+/* 11053번 가장 긴 증가하는 부분 수열 */
 
 public class Main {
 	static int N;
+	static int[] arr;
+	static int[] D;
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
-//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		Scanner sc = new Scanner(System.in);
-		int a, b;
+		N = Integer.parseInt(br.readLine());
+		int max = 0;
 		
-		while(sc.hasNextInt()) {
-			a = sc.nextInt();
-			b = sc.nextInt();
-			
-			System.out.println(a + b);
+		arr = new int[N];
+		D = new int[N];
+		
+		String[] strs = br.readLine().split(" ");
+		
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(strs[i]);
 		}
+		
+		Arrays.fill(D, 1);
+		
+		for (int i = 1; i < N; i++) {
+			max = 0;
+			for (int j = 0; j < i; j++) {
+				if (arr[j] < arr[i] && D[j] > max) {
+					D[i] = D[j] + 1;
+					max = D[j];
+				}				
+			}
+			
+//			System.out.println("D[" + i + "] : " + D[i]);
+		}
+		
+		max = 0;
+		for (int i = 0; i < N; i++) {
+			if (max < D[i]) {
+				max = D[i];
+			}
+		}
+		
+		System.out.println(max);
+		
+		
+		
 	}
 }
