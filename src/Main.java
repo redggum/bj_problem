da92009f08e2db12a2bed4470ca1b1269560b3e6
redggum <redggum@naver.com>
2019-06-26 23:14:33 +0900

    success

--- src/Main.java
+++ src/Main.java
@@ -1,77 +1,56 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.StringTokenizer;
 
-/* 11403번 경로 찾기 */
+/* 14501번 퇴사 */
 
 public class Main {
-	static int N;
-	static ArrayList<ArrayList<Integer>> list = new ArrayList<ArrayList<Integer>>();
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N;
+		int[] T;
+		int[] P;
+		int[] D;
+		int max;
+		StringTokenizer st;
 		
 		N = Integer.parseInt(br.readLine());
-		String[] strs;
-		int val = 0;
+		
+		T = new int[N];
+		P = new int[N];
+		D = new int[N + 1];
 		
 		for (int i = 0; i < N; i++) {
-			list.add(new ArrayList<Integer>());
+			st = new StringTokenizer(br.readLine());
+			T[i] = Integer.parseInt(st.nextToken());
+			P[i] = Integer.parseInt(st.nextToken());
 		}
 		
-		for (int i = 0; i < N; i++) {
-			strs = br.readLine().split(" ");
+		for (int i = 0; i <= N; i++) {
+			if (i == 0)
+			{
+				continue;
+			}
 			
-			for (int j = 0; j < strs.length; j++) {
-				val = Integer.parseInt(strs[j]);
-				if (val == 1) {
-					list.get(i).add(j);
-//					list.get(j).add(i);
+			max = 0;
+			for (int j = 0; j < i; j++) {
+//				System.out.println("i : " + i + ", j : " + j + ", T[" + j + "] : " + T[j]);
+				if (max < D[j]) {
+					max = D[j];
 				}
+				
+				if (j + T[j] == i && max < D[j] + P[j]) {
+					max = D[j] + P[j];
+//					System.out.println("max : " + max);
+				}
+				
+				D[i] = max;
+//				System.out.println("D[" + i + "] : " + D[i]);
 			}
 		}
 		
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				val = bfs(i, j) ? 1 : 0;
-				System.out.print(val + " ");
-			}
-			System.out.println();
-		}
+		System.out.println(D[N]);
 	}
-	
-	static boolean bfs(int from, int end) {
-		Queue<Integer> q = new LinkedList<Integer>();
-		int node = 0;
-		int nextNode = 0;
-		boolean[][] visited = new boolean[N][N];
-		
-		q.offer(from);
-		
-		while(!q.isEmpty()) {
-			node = q.poll();
-			
-			for (int i = 0; i < list.get(node).size(); i++) {
-				nextNode = list.get(node).get(i);
-				
-				if (visited[node][nextNode]) {
-					continue;
-				}
-				
-				if (nextNode == end) {
-					return true;
-				}
-				
-//				System.out.println("q.offer : " + nextNode);
-				q.offer(nextNode);
-				visited[node][nextNode] = true;
-//				visited[nextNode][node] = true;
-			}
-		}
-		
-		return false;
-	}
-}
+}	
