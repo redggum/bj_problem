8d30e0a65f634dda8e2d2ec70c4a59235a99353e
redggum <redggum@naver.com>
2019-08-15 00:07:34 +0900

    failure

--- src/Main.java
+++ src/Main.java
@@ -3,41 +3,54 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
-/* 2934번 LHR 식물 */
+/* 5012번 불만 정렬 */
 
 public class Main {
 
-	static int N, L, R;
+	static int N;
 	static int[] tree;
+	static int[] A;
+	static long[] t1;
+	static long[] t2;
+	static long result;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String[] strs;
 		
 		N = Integer.parseInt(br.readLine());
-		tree = new int[100000 + 1];
+		tree = new int[N + 1];
+		A = new int[N + 1];
+		t1 = new long[N + 1];
+		t2 = new long[N + 1];
+		result = 0;
 		
-		long sum1, sum2 = 0;
+		strs = br.readLine().split(" ");
 		
 		for (int i = 0; i < N; i++) {
-			strs = br.readLine().split(" ");
-			L = Integer.parseInt(strs[0]);
-			R = Integer.parseInt(strs[1]);
-			
-			sum1 = sum(L);
-			sum2 = sum(R);
-			
-			System.out.println(sum1 + sum2);
-			
-			update(L, (int)(0 - sum1));
-			update(L + 1, (int)sum1);
-			update(R, (int)(0 - sum2));
-			update(R + 1, (int)sum2);
-			
-			update(L + 1, 1);
-			update(R, -1);
+			A[i + 1] = Integer.parseInt(strs[i]);
 		}
 		
+		for (int i = 1; i <= N; i++) {
+			t1[i] = sum(N) - sum(A[i]);
+			update(A[i], 1);
+		}
+		
+		Arrays.fill(tree, 0);
+		
+		for (int i = N; i > 0; i--) {
+			t2[i] = sum(N) - sum(A[i]);
+			update(A[i], 1);
+		}			
+		
+		System.out.println("t1 : " + Arrays.toString(t1));
+		System.out.println("t2 : " + Arrays.toString(t2));
+		
+		for (int i = 1; i <= N; i++) {
+			result += t1[i] * t2[i];
+		}
+		
+		System.out.println(result);
 	}
 	
 	static long sum(int i) {
@@ -52,7 +65,7 @@ public class Main {
 
 	static void update(int i, int val) {
 
-		for (int x = i; x <= 100000; x += (x & -x)) {
+		for (int x = i; x <= N; x += (x & -x)) {
 			tree[x] += val;
 		}
 
