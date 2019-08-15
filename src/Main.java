81a23525843809e772360ead3a840f0ba51bed0d
redggum <redggum@naver.com>
2019-08-15 22:00:37 +0900

    success

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,8 @@ import java.util.StringTokenizer;
 public class Main {
 
 	static int N, M;
-	static long[] tree;
+	static long[] treeA;
+	static long[] treeB;
 	static int[] arr;
 	
 	static final long INF = Long.MAX_VALUE;
@@ -20,7 +21,8 @@ public class Main {
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		
-		tree = new long[4 * N + 1];
+		treeA = new long[4 * N + 1];
+		treeB = new long[4 * N + 1];
 		arr = new int[N + 1];
 		int tmp;
 		
@@ -29,7 +31,8 @@ public class Main {
 			arr[i] = tmp;
 		}
 		
-		init(1, 1, N);
+		initA(1, 1, N);
+		initB(1, 1, N);
 		
 //		System.out.println(Arrays.toString(tree));
 		
@@ -41,43 +44,55 @@ public class Main {
 			a = Integer.parseInt(st.nextToken());
 			b = Integer.parseInt(st.nextToken());
 			
-			System.out.println(m(1, 1, N, a, b));
+			System.out.print(mA(1, 1, N, a, b) + " " + mB(1, 1, N, a, b));
+			System.out.println();
 		}
 	}
 	
-	static long init(int node, int s, int e) {
+	static long initA(int node, int s, int e) {
 		if (s == e) {
-			return tree[node] = arr[s];
+			return treeA[node] = arr[s];
 		}
 		
-		return tree[node] = min(init(node * 2, s, (s + e) / 2), init(node * 2 + 1, (s + e) / 2 + 1, e));
+		return treeA[node] = min(initA(node * 2, s, (s + e) / 2), initA(node * 2 + 1, (s + e) / 2 + 1, e));
 	}
 	
-	static void update(int node, int s, int e, int v, int d) {
-		if (v < s || v > e) {
-			return;
+	static long initB(int node, int s, int e) {
+		if (s == e) {
+			return treeB[node] = arr[s];
 		}
 		
-		tree[node] += d;
-		
-		if (s != e) {
-			update(node * 2, s, (s + e) / 2, v, d);
-			update(node * 2 + 1, (s + e) / 2 + 1, e, v, d);
-		}
+		return treeB[node] = max(initB(node * 2, s, (s + e) / 2), initB(node * 2 + 1, (s + e) / 2 + 1, e));
+
 	}
 	
-	static long m(int node, int s, int e, int l, int r) {
+	static long mA(int node, int s, int e, int l, int r) {
 		if (l > e || r < s) {
 			return INF;
-		} else if (l <= s && e <= r) {
-			return tree[node];
+		} else if (l <= s && r >= e) {
+			return treeA[node];
 		}
 		
-		return min(m(node * 2, s, (s + e) / 2, l, r), m(node * 2 + 1, (s + e) / 2 + 1, e, l, r));
+		return min(mA(node * 2, s, (s + e) / 2, l, r), mA(node * 2 + 1, (s + e) / 2 + 1, e, l, r));
+	}
+	
+	static long mB(int node, int s, int e, int l, int r) {
+		if (l > e || r < s) {
+			return 0;
+		} else if (l <= s && r >= e) {
+			return treeB[node];
+		}
+		
+		return max(mB(node * 2, s, (s + e) / 2, l, r), mB(node * 2 + 1, (s + e) / 2 + 1, e, l, r));
 	}
 	
 	static long min(long a, long b) {
 		if (a > b) return b;
 		else return a;
+	}
+	
+	static long max(long a, long b) {
+		if (a > b) return a;
+		else return b;
 	}
 }	
