75a5ced5331aeeb3df5aead7ff9b65fb78528a1e
redggum <redggum@naver.com>
2019-08-15 10:43:13 +0900

    success

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,17 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
-import javax.swing.plaf.synth.SynthSeparatorUI;
-
-/* 2042번 구간 합 구하기 */
+/* 10868번 최솟값 */
 
 public class Main {
 
-	static int N, M, K;
+	static int N, M;
 	static long[] tree;
 	static int[] arr;
+	
+	static final long INF = Long.MAX_VALUE;
 	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -20,7 +19,6 @@ public class Main {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		K = Integer.parseInt(st.nextToken());
 		
 		tree = new long[4 * N + 1];
 		arr = new int[N + 1];
@@ -35,61 +33,51 @@ public class Main {
 		
 //		System.out.println(Arrays.toString(tree));
 		
-		int a, b, c;
+		int a, b;
 		
-		for (int i = 0; i < M + K; i++) {
+		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
 
 			a = Integer.parseInt(st.nextToken());
 			b = Integer.parseInt(st.nextToken());
-			c = Integer.parseInt(st.nextToken());
 			
-			switch(a) {
-			case 1 :
-				update(1, 1, N, b, c - arr[b]);
-				arr[b] = c;
-				break;
-			case 2 :
-				System.out.println(sum(1, 1, N, b, c));
-				break;
-			}
+			System.out.println(m(1, 1, N, a, b));
 		}
 	}
 	
 	static long init(int node, int s, int e) {
-//		System.out.println("node : " + node + ", s : " + s + ", e : " + e);
 		if (s == e) {
-//			System.out.println("last one - node : " + node + ", s : " + s);
 			return tree[node] = arr[s];
 		}
 		
-		return tree[node] = init(node * 2, s, (s + e) / 2) + init(node * 2 + 1, (s + e) / 2 + 1, e);
+		return tree[node] = min(init(node * 2, s, (s + e) / 2), init(node * 2 + 1, (s + e) / 2 + 1, e));
 	}
 	
-	static void update(int node, int s, int e, int target, int newVal) {
-//		System.out.println("node : " + node + ", s : " + s + ", e : " + e + ", target : " + target + ", newVal : " + newVal);
-		if (target < s || target > e) {
+	static void update(int node, int s, int e, int v, int d) {
+		if (v < s || v > e) {
 			return;
 		}
 		
-		tree[node] += newVal;
+		tree[node] += d;
 		
 		if (s != e) {
-			update(node * 2, s, (s + e) / 2, target, newVal);
-			update(node * 2 + 1, (s + e) / 2 + 1, e, target, newVal);
+			update(node * 2, s, (s + e) / 2, v, d);
+			update(node * 2 + 1, (s + e) / 2 + 1, e, v, d);
 		}
 	}
 	
-	static long sum(int node, int s, int e, int l, int r) {
-		
+	static long m(int node, int s, int e, int l, int r) {
 		if (l > e || r < s) {
-			return 0;
-		}
-		
-		if (l <= s && r >= e) {
+			return INF;
+		} else if (l <= s && e <= r) {
 			return tree[node];
 		}
 		
-		return sum(node * 2, s, (s + e) / 2, l, r) + sum(node * 2 + 1, (s + e) / 2 + 1, e, l, r);
+		return min(m(node * 2, s, (s + e) / 2, l, r), m(node * 2 + 1, (s + e) / 2 + 1, e, l, r));
+	}
+	
+	static long min(long a, long b) {
+		if (a > b) return b;
+		else return a;
 	}
 }	
