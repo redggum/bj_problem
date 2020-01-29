4f91abe25f1c4272e6e17800759f3e60894dc609
redggum <redggum@naver.com>
2020-01-30 02:28:03 +0900

    suc

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,35 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Scanner;
 
-/* 10871번 X보다 작은 수 */
+/* 10171번 고양이 */
 
 public class Main {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		String[] strs = br.readLine().split(" ");
-		
-		int a = Integer.parseInt(strs[0]);
-		int x = Integer.parseInt(strs[1]);
-		
-		int[] arr = new int[a + 1];
-		String str = "";
-		
-		strs = br.readLine().split(" ");
-		
-		for (int i = 0; i < a; i++) {
-			arr[i] = Integer.parseInt(strs[i]);
-			
-			if (arr[i] < x) {
-				str += arr[i] + " ";
-			}
-		}
-		
-		System.out.println(str);
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		
+//		String[] strs = br.readLine().split(" ");
+//		
+//		int a = Integer.parseInt(strs[0]);
+//		int x = Integer.parseInt(strs[1]);
+//		
+//		int[] arr = new int[a + 1];
+//		String str = "";
+//		
+//		strs = br.readLine().split(" ");
+//		
+//		for (int i = 0; i < a; i++) {
+//			arr[i] = Integer.parseInt(strs[i]);
+//			
+//			if (arr[i] < x) {
+//				str += arr[i] + " ";
+//			}
+//		}
+//		
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( \')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
 	}
 }
