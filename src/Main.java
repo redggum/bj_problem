c49eede2234cea027e9782ceaa968ea65c157d49
redggum <redggum@naver.com>
2020-02-03 22:58:44 +0900

    suc

--- src/Main.java
+++ src/Main.java
@@ -1,98 +1,28 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.LinkedList;
-import java.util.Queue;
 
-/* 1967번 트리의 지름 */
+/* 1330번 두 수 비교하기 */
 
 public class Main {
 
-	static int N, M;
-	static ArrayList<Node>[] arr;
-	
-	static final long INF = Long.MAX_VALUE;
-	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String[] strs;
-		int a, b, c;
-		int[] dist;
-		Queue<Integer> q;
-		boolean[] visit;
-		ArrayList<Node> res;
+
+		int a, b;
 		
-		N = Integer.parseInt(br.readLine());
+		strs = br.readLine().split(" ");
 		
-		arr = new ArrayList[N + 1];
+		a = Integer.parseInt(strs[0]);
+		b = Integer.parseInt(strs[1]);
 		
-		for (int i = 0; i < arr.length; i++) {
-			arr[i] = new ArrayList<>();
+		if (a > b) {
+			System.out.println(">");
+		} else if (a < b) {
+			System.out.println("<");
+		} else {
+			System.out.println("==");
 		}
-		
-		dist = new int[N + 1];
-		q = new LinkedList<Integer>();
-		visit = new boolean[N + 1];
-		res = new ArrayList<>();
-		
-		for (int i = 1; i < N; i++) {
-			strs = br.readLine().split(" ");
-			a = Integer.parseInt(strs[0]);
-			b = Integer.parseInt(strs[1]);
-			c = Integer.parseInt(strs[2]);
-			
-			arr[a].add(new Node(b, c));
-			arr[b].add(new Node(a, c));			
-		}
-		
-		q.offer(1);
-		visit[1] = true;
-		dist[1] = 0;
-		
-		while(!q.isEmpty()) {
-			int v = q.poll();
-			
-			res.add(new Node(v, dist[v]));
-			
-			
-			
-			visit[v] = true;
-			
-			System.out.println("v : " + v);
-			
-			for (int i = 0; i < arr[v].size(); i++) {
-				Node n = arr[v].get(i);
-				if (visit[n.v]) {
-					continue;
-				}
-				
-				dist[n.v] = dist[v] + n.e;
-				q.offer(n.v);
-			}
-		}
-		
-		res.sort(new Comparator<Node>() {
-			@Override
-			public int compare(Node o1, Node o2) {
-				return o2.e - o1.e;
-			}
-		});
-		
-		
-		System.out.println(res.get(0).e + res.get(1).e);
 	}
 }	
-
-class Node {
-	int v;
-	int e;
-	
-	Node(int v, int e) {
-		this.v = v;
-		this.e = e;
-	}
-}
