6c9c56a7c95e61400ce735abca8c144d0ca7b8ca
redggum <redggum@naver.com>
2020-02-04 00:05:04 +0900

    suc

--- src/Main.java
+++ src/Main.java
@@ -2,25 +2,26 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-/* 11022번 A+B - 8 */
+/* 2588번 곱셈 */
 
 public class Main {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String[] strs;
 
 		int a, b;
 
-		int CASE = Integer.parseInt(br.readLine());
-
-		for (int i = 1; i <= CASE; i++) {
-			strs = br.readLine().split(" ");
-
-			a = Integer.parseInt(strs[0]);
-			b = Integer.parseInt(strs[1]);
-
-			System.out.println("Case #" + i + ": " + a + " + " + b + " = " + (a + b));
-		}
+		a = Integer.parseInt(br.readLine());
+		b = Integer.parseInt(br.readLine());
+		
+		int fst, snd, trd;
+		fst = b / 100;
+		snd = (b - fst * 100) / 10;
+		trd = b % 10;
+		
+		System.out.println(trd = a * trd);
+		System.out.println(snd = a * snd);
+		System.out.println(fst = a * fst);
+		System.out.println(fst * 100 + snd * 10 + trd);
 	}
 }
