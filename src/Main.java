4490dbb17b1ebbf4161c600884f1832e5eb070a5
redggum <redggum@naver.com>
2020-02-03 23:06:50 +0900

    suc

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-/* 1330번 두 수 비교하기 */
+/* 11021번 A+B - 7 */
 
 public class Main {
 
@@ -11,18 +11,16 @@ public class Main {
 		String[] strs;
 
 		int a, b;
-		
-		strs = br.readLine().split(" ");
-		
-		a = Integer.parseInt(strs[0]);
-		b = Integer.parseInt(strs[1]);
-		
-		if (a > b) {
-			System.out.println(">");
-		} else if (a < b) {
-			System.out.println("<");
-		} else {
-			System.out.println("==");
+
+		int CASE = Integer.parseInt(br.readLine());
+
+		for (int i = 1; i <= CASE; i++) {
+			strs = br.readLine().split(" ");
+
+			a = Integer.parseInt(strs[0]);
+			b = Integer.parseInt(strs[1]);
+
+			System.out.println("Case #" + i + ": " + (a + b));
 		}
 	}
-}	
+}
