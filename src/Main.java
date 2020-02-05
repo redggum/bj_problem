df13e41f8f497933fafde01eb3902dff5ceb7197
redggum <redggum@naver.com>
2020-02-05 23:34:38 +0900

    suc

--- src/Main.java
+++ src/Main.java
@@ -3,51 +3,31 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
 
-/* 10828번 스택 */
+/* 2884번 알람 시계 */
 
 public class Main {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		int N;
-		String cmd;
-		int num;
-		int index = -1;
-		Stack<Integer> stack = new Stack<Integer>();
+		String[] strs  = br.readLine().split(" ");
+		int H = Integer.parseInt(strs[0]);
+		int M = Integer.parseInt(strs[1]);
+		int mH = 0;
 		
-		N = Integer.parseInt(br.readLine());
-		
-		for(int i = 1; i <= N; i++) {
-			String[] strs = br.readLine().split(" "); 
-			cmd = strs[0];
+		if (M - 45 < 0) {
+			M = 60 - (45 - M);
+			mH = 1;
 			
-			if(cmd.equals("push")) {
-				num = Integer.parseInt(strs[1]);
-				stack.push(num);
-			} else if(cmd.equals("pop")) {
-				if(stack.isEmpty()) {
-					System.out.println(-1);
-				} else {
-					System.out.println(stack.peek());
-					stack.pop();
-				}
-			} else if(cmd.equals("size")) {
-				System.out.println(stack.size());
-			} else if(cmd.equals("empty")) {
-				if(stack.isEmpty()) {
-					System.out.println(1);
-				} else {
-					System.out.println(0);
-				}
-			} else if(cmd.equals("top")) {
-//				System.out.println("size : " + stack.size());
-				if(stack.isEmpty()) {
-					System.out.println(-1);
-				} else {
-					System.out.println(stack.peek());
-				}
+			if (H - 1 < 0) {
+				H = 23;
+			} else {
+				H -= mH;
 			}
+		} else {
+			M -= 45;
 		}
+		
+		System.out.println(H + " " + M);
 	}
 }
