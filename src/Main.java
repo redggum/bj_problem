08d0d290998c619be494187e95880bd7cd2bdf73
redggum <redggum@naver.com>
2020-02-10 22:56:18 +0900

    suc

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,32 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
 
-/* 1003번 피보나치 함수 */
+/* 10818번 최소, 최대 */
 
 public class Main {
-	static int n0;
-	static int n1;
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		int T = Integer.parseInt(br.readLine());
-		int N;
-		int[][] D; 
-
-		for (int tc = 1; tc <= T; tc++) {
-			N = Integer.parseInt(br.readLine());
+		int N = Integer.parseInt(br.readLine());
+		String[] strs = br.readLine().split(" ");
+		
+		int min = Integer.MAX_VALUE;
+		int max = Integer.MIN_VALUE;
+		
+		for (String str : strs) {
+			int num = Integer.parseInt(str);
 			
-			D = new int[50][2];
-			
-			D[0][0] = 1;
-			D[1][1] = 1;
-			
-			for (int i = 2; i <= N; i++) {
-				D[i][1] = D[i-1][1] + D[i-2][1];
-				D[i][0] = D[i-1][0] + D[i-2][0];
+			if (min > num) {
+				min = num;
 			}
 			
-			System.out.println(D[N][0] + " " + D[N][1]);
+			if (max < num) {
+				max = num;
+			}
 		}
+		
+		System.out.println(min + " " + max);
 	}
 }
