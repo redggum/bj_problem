874d6dad56fcbdae1b773e2238bafc5386f773d8
redggum <redggum@naver.com>
2020-02-13 23:56:17 +0900

    suc

--- src/Main.java
+++ src/Main.java
@@ -1,41 +1,25 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
 
-/* 2751번 수 정렬하기 2 */
+/* 3052번 나머지 */
 
 public class Main {
-	static int N;
+	static int DIV = 42;
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		N = Integer.parseInt(br.readLine());
-
-		int[] minus = new int[1000000 + 1];
-		int[] plus = new int[1000000 + 1];
-		int num = 0;
-
-		for (int i = 0; i < N; i++) {
-			num = Integer.parseInt(br.readLine());
-
-			if (num < 0) {
-				minus[Math.abs(num)] = 1;
-			} else {
-				plus[num] = 1;
-			}
-		}
-
-		for (int i = 1000000; i > 0; i--) {
-			if (minus[i] == 1) {
-				System.out.println(0 - i);
-			}
+		Set<Integer> numset = new HashSet<Integer>();
+		
+		for (int i = 0; i < 10; i++) {
+			int num = Integer.parseInt(br.readLine());
+			
+			numset.add(num % DIV);
 		}
 		
-		for (int i = 0; i <= 1000000; i++) {
-			if (plus[i] == 1) {
-				System.out.println(i);
-			}
-		}
+		System.out.println(numset.size());
 	}
 }
