828e65e94de610a90b730fe20decd22f0a466888
redggum <redggum@naver.com>
2020-02-13 23:48:17 +0900

    suc

--- src/Main.java
+++ src/Main.java
@@ -11,33 +11,37 @@ public class Main {
 		int N = Integer.parseInt(br.readLine());
 		
 		int[] val = new int[N + 1];
-		int[][] d = new int[N + 1][2];
+		int[] d = new int[N + 1];
 
 		val[1] = Integer.parseInt(br.readLine());
+		
+		if (N == 1) {
+			System.out.println(val[1]);
+			return;
+		}
+		
+		
 		val[2] = Integer.parseInt(br.readLine());
 		
-		d[1][0] = 0;
-		d[1][1] = val[1];
+		d[1] = val[1];
+		d[2] = d[1] + val[2];
 		
-		d[2][0] = d[1][1];
-		d[2][1] = max(val[2] + d[1][1], val[2] + d[1][0]);
-		
-//		System.out.println("d[" + 1 + "][0] : " + d[1][0]);
-//		System.out.println("d[" + 1 + "][1] : " + d[1][1]);
-//		System.out.println("d[" + 2 + "][0] : " + d[2][0]);
-//		System.out.println("d[" + 2 + "][1] : " + d[2][1]);
+		int a, b, c;
 		
 		for (int i = 3; i <= N; i++) {
 			val[i] = Integer.parseInt(br.readLine());
 			
-			d[i][0] = max(d[i - 1][1], d[i - 1][0]);
-			d[i][1] = d[i -1][1] > d[i - 2][1] ? max(val[i] + val[i - 1] + d[i - 2][0], val[i] + d[i - 2][1]) : val[i] + d[i - 1][0];
+			a = d[i - 2] + val[i];
+			b = d[i - 3] + val[i - 1] + val[i];
+			c = d[i - 1];
 			
-//			System.out.println("d[" + i + "][0] : " + d[i][0]);
-//			System.out.println("d[" + i + "][1] : " + d[i][1]);
+//			System.out.println(a + ", " + b + ", " + c);
+			
+			d[i] = max(a, b);
+			d[i] = max(c, d[i]);
 		}
 		
-		System.out.println(max(d[N][0], d[N][1]));
+		System.out.println(d[N]);
 	}
 	
 	public static int max(int a, int b) {
