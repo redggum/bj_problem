216ff642c9d384a379e166c55b3efd58ad1aedf2
redggum <redggum@naver.com>
2020-02-24 00:24:02 +0900

    fail

--- src/Main.java
+++ src/Main.java
@@ -12,17 +12,16 @@ public class Main {
 	static int T, M, N, K;
 	static ArrayList<Integer> arr;
 	static int[] parent;
+	static boolean[] visited;
+	static boolean[] valid;
+	static int[] moveX = { 1, 0, -1, 0 };
+	static int[] moveY = { 0, 1, 0, -1 };
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		String[] strs;
 		int x, y;
-		int[] moveX = { 1, 0, -1, 0 };
-		int[] moveY = { 0, 1, 0, -1 };
-		int nextX, nextY;
-		boolean[] visited;
-		Queue<Integer> q;
 
 		T = Integer.parseInt(br.readLine());
 
@@ -35,15 +34,13 @@ public class Main {
 
 			// init;
 			visited = new boolean[M * N + 1];
-			q = new LinkedList<Integer>();
+			valid = new boolean[M * N + 1];
+
 			Arrays.fill(visited, false);
 
 			arr = new ArrayList<Integer>();
 
-			parent = new int[M * N + 1];
-
 			int xy;
-			int nextXy;
 
 			for (int i = 0; i < K; i++) {
 				strs = br.readLine().split(" ");
@@ -53,84 +50,58 @@ public class Main {
 
 				xy = getXY(x, y);
 				arr.add(xy);
-
-				parent[xy] = xy;
-				
-//				System.out.println("x : " + x + ", y : " + y + ", xy : " + xy);
+				valid[xy] = true;
 			}
 
 			int sum = 0;
-			
+
 			for (int i = 0; i < K; i++) {
 				xy = arr.get(i);
-				
-//				System.out.println(visited[xy]);
 
 				if (visited[xy] == true) {
 					continue;
 				}
 
-				q.offer(xy);
-				visited[xy] = true;
-				
-				sum++;
-
-				while (!q.isEmpty()) {
-					xy = q.poll();
-
-					for (int j = 0; j < 4; j++) {
-						nextX = getX(xy) + moveX[j];
-						nextY = getY(xy) + moveY[j];
-						
-						if (nextX < 0 || nextX >= M || nextY < 0 || nextY >= N || visited[nextXy = getXY(nextX, nextY)]) {
-							continue;
-						}
-						
-						
-						if(parent[nextXy] == 0) {
-							continue;
-						}
-
-						q.offer(nextXy);
-						visited[nextXy] = true;
-						
-//						System.out.println("nextX : " + nextX + ", nextY : " + nextY);
-						
-//						if (find(xy) != find(nextXy)) {
-////							System.out.println("nextXy : " + nextXy + ", xy : " + xy);
-////							System.out.println("parent of nextXy : " + parent[nextXy] + ", parent of xy : " + parent[xy]);
-//							parent[nextXy] = parent[xy];
-//							q.offer(nextXy);
-//							visited[nextXy] = true;
-//						}
-					}
-
-				}
+				sum++;				
+				System.out.println("xy : " + xy);
+				bfs(xy);
 			}
-
-//			int sum = 0;
-//			Set<Integer> tmp = new HashSet<Integer>();
-//			int val;
-//			
-//			for (int i = 0; i < K; i++) {
-//				val = parent[arr.get(i)];
-////				System.out.println("val : " + val);
-//				if (!tmp.contains(val)) {
-//					sum++;
-//					tmp.add(val);
-//				}
-//			}
 
 			System.out.println(sum);
 		}
 	}
 
-	static int find(int xy) {
-		if (parent[xy] == xy) {
-			return xy;
-		}
+	static void bfs(int xy) {
+		Queue<Integer> q = new LinkedList<Integer>();
+		int nextX, nextY;
+		int nextXy;
 
-		return find(parent[xy]);
+		q.offer(xy);
+		visited[xy] = true;
+
+		while (!q.isEmpty()) {
+			xy = q.poll();
+
+			for (int j = 0; j < 4; j++) {
+				nextX = getX(xy) + moveX[j];
+				nextY = getY(xy) + moveY[j];
+				nextXy = getXY(nextX, nextY);
+
+//				if (nextX >= 0 && nextX < M && nextY >= 0 && nextY < N) {
+//					System.out.println("nextXy : " + nextXy + ", visited[nextXy] :" + visited[nextXy]
+//							+ ", valid[nextXy] :" + valid[nextXy]);
+//				}
+
+				if (nextX < 0 || nextX >= M || nextY < 0 || nextY >= N || visited[nextXy] || valid[nextXy] == false) {
+					continue;
+				}
+
+				q.offer(nextXy);
+				visited[nextXy] = true;
+
+//				System.out.println("nextX : " + nextX + ", nextY : " + nextY + ", nextXy : " + nextXy);
+			}
+		}
 	}
 
 	static int getXY(int x, int y) {
