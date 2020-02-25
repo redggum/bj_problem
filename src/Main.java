59cedb6eb6700c99d50400bca203537e8248ddd8
redggum <redggum@naver.com>
2020-02-25 23:47:53 +0900

    suc

--- src/Main.java
+++ src/Main.java
@@ -6,99 +6,33 @@ import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 
-/* 1012번 유기농 배추 */
+/* 9461번 파도반 수열 */
 
 public class Main {
-	static int T, M, N, K;
-	static ArrayList<String> arr;
-	static int[] parent;
-	static boolean[][] visited;
-	static boolean[][] valid;
-	static int[] moveX = { 1, 0, -1, 0 };
-	static int[] moveY = { 0, 1, 0, -1 };
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		String[] strs;
-		int x, y;
+		int T = Integer.parseInt(br.readLine());
+		int N;
+		long[] D;
 
-		T = Integer.parseInt(br.readLine());
-
-		for (int tc = 1; tc <= T; tc++) {
-			strs = br.readLine().split(" ");
-
-			M = Integer.parseInt(strs[0]);
-			N = Integer.parseInt(strs[1]);
-			K = Integer.parseInt(strs[2]);
-
-			// init;
-			visited = new boolean[M][N];
-			valid = new boolean[M][N];
-
-			arr = new ArrayList<String>();
-
-			for (int i = 0; i < K; i++) {
-				strs = br.readLine().split(" ");
-
-				x = Integer.parseInt(strs[0]);
-				y = Integer.parseInt(strs[1]);
-
-				arr.add(x + ":" + y);
-				valid[x][y] = true;
-			}
-
-			int sum = 0;
-
-			for (int i = 0; i < K; i++) {
-				String[] st = arr.get(i).split(":");
-				x = Integer.parseInt(st[0]);
-				y = Integer.parseInt(st[1]);
-
-				if (visited[x][y] == true) {
-					continue;
-				}
-
-				sum++;				
-				bfs(x, y);
-			}
-
-			System.out.println(sum);
+		D = new long[100 + 1];
+		
+		D[1] = 1;
+		D[2] = 1;
+		D[3] = 1;
+		D[4] = 2;
+		D[5] = 2;
+		
+		for (int n = 6; n <= 100; n++) {
+			D[n] = D[n - 1] + D[n - 5];
 		}
-	}
+		
+		for (int tc = 1; tc <= T; tc++) {
+			N = Integer.parseInt(br.readLine());
 
-	static void bfs(int x, int y) {
-		Queue<String> q = new LinkedList<String>();
-		int nextX, nextY;
-
-		q.offer(x + ":" + y);
-		visited[x][y] = true;
-
-		while (!q.isEmpty()) {
-			String[] st = q.poll().split(":");
-
-			for (int j = 0; j < 4; j++) {
-				nextX = Integer.parseInt(st[0]) + moveX[j];
-				nextY = Integer.parseInt(st[1]) + moveY[j];
-
-//				if (nextX >= 0 && nextX < M && nextY >= 0 && nextY < N) {
-//					System.out.println("nextXy : " + nextXy + ", visited[nextXy] :" + visited[nextXy]
-//							+ ", valid[nextXy] :" + valid[nextXy]);
-//				}
-
-				if (nextX < 0 || nextX >= M || nextY < 0 || nextY >= N) {
-					continue;
-				}
-				
-				if (visited[nextX][nextY] || valid[nextX][nextY] == false) {
-					continue;
-				}
-
-				q.offer(nextX + ":" + nextY);
-				visited[nextX][nextY] = true;
-
-//				System.out.println("nextX : " + nextX + ", nextY : " + nextY + ", nextXy : " + nextXy);
-			}
+			System.out.println(D[N]);
 		}
 	}
 }
