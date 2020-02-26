fad4b2fd03070459a3ab678981a20d7bc38bc003
redggum <redggum@naver.com>
2020-02-27 00:39:19 +0900

    suc

--- src/Main.java
+++ src/Main.java
@@ -2,52 +2,38 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-/* 2581번 소수 */
+/* 2775번 부녀회장이 될테야 */
 
 public class Main {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		int m, n;
-		long sum = 0;
-		int min = 10001;
-
-		m = Integer.parseInt(br.readLine());
-		n = Integer.parseInt(br.readLine());
-
-		for (int i = m; i <= n; i++) {
-			if (isDecimal(i)) {
-//				System.out.println(i);
-				sum += i;
-
-				if (i < min) {
-					min = i;
+		int T, K, N;
+		
+		long[][] D;
+		
+		T = Integer.parseInt(br.readLine());
+		
+		for (int t = 1; t <= T; t++) {
+			K = Integer.parseInt(br.readLine());
+			N = Integer.parseInt(br.readLine());
+			
+			D = new long[K + 1][N + 1];
+			
+			for (int i = 1; i <= N; i++) {
+				D[0][i] = i;
+			}
+			
+			for (int k = 1; k <= K; k++) {
+				for (int n = 1; n <= N; n++) {
+					for (int m = 1; m <= n; m++) {
+						D[k][n] += D[k -1][m];
+					}
 				}
 			}
+			
+			System.out.println(D[K][N]);
 		}
-
-		if (sum > 0) {
-			System.out.println(sum);
-			System.out.println(min);
-		} else {
-			System.out.println(-1);
-		}
-	}
-
-	static boolean isDecimal(int n) {
-		if (n == 1) {
-			return false;
-		} else if (n != 2 && n % 2 == 0) {
-			return false;
-		} else {
-			for (int i = 3; i < n; i += 2) {
-				if (n % i == 0) {
-					return false;
-				}
-			}
-		}
-
-		return true;
 	}
 }
