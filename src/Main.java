3ffb4cb01794b5dd09acf2d7b4c8168957724730
redggum <redggum@naver.com>
2020-02-26 23:45:34 +0900

    suc

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,53 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
 
-/* 1934번 최소공배수 */
+/* 2581번 소수 */
 
 public class Main {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		int T = Integer.parseInt(br.readLine());
-		String[] strs;
-		int a, b;
-		
-		for (int tc = 1; tc <= T; tc++) {
-			strs = br.readLine().split(" ");
-			a = Integer.parseInt(strs[0]);
-			b = Integer.parseInt(strs[1]);
+		int m, n;
+		long sum = 0;
+		int min = 10001;
 
-			System.out.println(a * b / gcd(a, b));
+		m = Integer.parseInt(br.readLine());
+		n = Integer.parseInt(br.readLine());
+
+		for (int i = m; i <= n; i++) {
+			if (isDecimal(i)) {
+//				System.out.println(i);
+				sum += i;
+
+				if (i < min) {
+					min = i;
+				}
+			}
+		}
+
+		if (sum > 0) {
+			System.out.println(sum);
+			System.out.println(min);
+		} else {
+			System.out.println(-1);
 		}
 	}
-	
-	static int gcd(int a, int b){
-		while(b != 0) {
-			int r = a % b;
-			a = b;
-			b = r;
-		}
-		return a;
-	}
 
+	static boolean isDecimal(int n) {
+		if (n == 1) {
+			return false;
+		} else if (n != 2 && n % 2 == 0) {
+			return false;
+		} else {
+			for (int i = 3; i < n; i += 2) {
+				if (n % i == 0) {
+					return false;
+				}
+			}
+		}
+
+		return true;
+	}
 }
